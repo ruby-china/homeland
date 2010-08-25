441281faf02ed9b49ab1b93218e4cf7b74f26a6b
Jason Lee <huacnlee@gmail.com>
2010-08-25 16:35:32 +0800

    改进了错误与成功的提示样式，并设置好后台提示的位置

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # return the formatted flash[:notice] html
+  def notice_message()
+    if flash[:notice]
+      result = '<div id="success_message">'+flash[:notice]+'</div>'
+    else
+      result = ''
+    end
+    
+    return raw(result)
+  end
 end

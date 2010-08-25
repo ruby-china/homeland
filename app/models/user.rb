0084d79db299e638cd74febcb6d1131a111b4789
Jason Lee <huacnlee@gmail.com>
2010-08-25 15:58:41 +0800

    修正大背景图片定位的问题

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   validates_presence_of :email, :name, :passwd
   validates_uniqueness_of :email, :name
+  validates_format_of     :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
   has_many :topics
   has_many :replies
   

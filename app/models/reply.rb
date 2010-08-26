ca4b08260b6c46d8bda6771345af9584a5a0cff9
Jason Lee <huacnlee@gmail.com>
2010-08-26 13:10:19 +0800

    save last_reply_user after Topic reply

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -2,4 +2,14 @@
 class Reply < ActiveRecord::Base
   belongs_to :topic, :counter_cache => true
   belongs_to :user
+
+  after_create :update_parent_last_replied
+  def update_parent_last_replied
+    self.topic.replied_at = Time.now
+    self.topic.last_reply_user_id = self.user_id
+    self.topic.save
+
+    # 清除用户读过记录
+    self.topic.clear_user_readed
+  end
 end

e7ec42927e9b74f15a4b22c69186835460e8178c
Jason Lee <huacnlee@gmail.com>
2010-08-30 23:47:18 +0800

    Fixed ruby file utf8 bug

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -1,3 +1,4 @@
+# coding: utf-8  
 class BaseMailer < ActionMailer::Base
   default :from => APP_CONFIG['smtp_username']
   default_url_options[:host] = APP_CONFIG['domain']
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,4 @@
+# coding: utf-8  
 class UserMailer < BaseMailer  
   def welcome(user)
     @user = user

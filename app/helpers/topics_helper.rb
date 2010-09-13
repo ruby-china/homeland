f728f9586ec38dda02fa0d040c2f9363df3b65e9
Jason Lee <huacnlee@gmail.com>
2010-09-13 21:10:27 +0800

    Youku/Tudou/Ku6 video insert support

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -12,6 +12,21 @@ module TopicsHelper
           # text = text.gsub(img_url, img_url.gsub("large","small"))
         # end
       # end
+      # Youku
+      text = text.gsub(/http:\/\/player\.youku\.com\/player\.php\/sid\/([\w]+)\/v\.swf/i,
+        '<embed src="http://player.youku.com/player.php/sid/\1/v.swf" 
+          quality="high" width="480" height="400" align="middle" 
+          allowScriptAccess="sameDomain" type="application/x-shockwave-flash"></embed>')
+      # Tudou
+      text = text.gsub(/http:\/\/www\.tudou\.com\/[v|l]\/([\-\w]+)[\/v\.swf]{0,6}/i,
+        '<embed src="http://www.tudou.com/v/\1/v.swf" 
+        type="application/x-shockwave-flash" allowscriptaccess="always" 
+        allowfullscreen="true" wmode="opaque" width="480" height="400"></embed>')
+      # Ku6
+      text = text.gsub(/http:\/\/player\.ku6\.com\/refer\/([\-_\w]+)\/v\.swf/i,
+        '<embed src="http://player.ku6.com/refer/\1/v.swf" 
+        type="application/x-shockwave-flash" allowscriptaccess="always" 
+        allowfullscreen="true" wmode="opaque" width="480" height="400"></embed>')
     end
     text = auto_link(text,:all, :target => '_blank', :rel => "nofollow")
     text = text.gsub(/@([\d]+)楼\s/,'@<a href="#reply\1" onclick="return hightlightReply(\1)">\1楼</a> ')

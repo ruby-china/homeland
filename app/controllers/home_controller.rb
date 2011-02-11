99914ca99a504723f2f4f868b3a7127070356814
Jason Lee <huacnlee@gmail.com>
2011-02-11 13:14:14 +0800

    Change login style . Add bind account in Setting page. Fix auth account login return back path.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -33,18 +33,22 @@ class HomeController < ApplicationController
 
 	def auth_callback
 		auth = request.env["omniauth.auth"]  
+		redirect_to root_path if auth.blank?
+
 		@auth = Authorization.find_from_hash(auth)
 		if current_user
-      flash[:notice] = "Successfully added #{auth['provider']} authentication"
       current_user.authorizations.create(:provider => auth['provider'], :uid => auth['uid']) #Add an auth to existing
+      flash[:notice] = "成功绑定了 #{auth['provider']} 帐号。"
+			redirect_to setting_path
 		elsif @auth
-			flash[:notice] = "Welcome back #{auth['provider']} user"
       UserSession.create(@auth.user, true) #User is present. Login the user with his social account
+			flash[:notice] = "登陆成功。"
+			redirect_to root_url
 		else
 			@new_auth = Authorization.create_from_hash(auth, current_user) #Create a new user
-      flash[:notice] = "Welcome #{auth['provider']} user. Your account has been created."
       UserSession.create(@new_auth.user, true) #Log the authorizing user in.
+      flash[:notice] = "欢迎来自 #{auth['provider']} 的用户，你的帐号已经创建成功。"
+			redirect_to root_url
 		end
-		redirect_to root_url
 	end
 end

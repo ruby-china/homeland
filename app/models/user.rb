e23f3d56d436c81ed355489ba8f2fa8158a654fe
Jason Lee <huacnlee@gmail.com>
2011-02-21 22:16:02 +0800

    Fixed Mongoid:Paperclip not existed bug, removed it, not need now.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
-  include Mongoid::Paperclip
-  
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable

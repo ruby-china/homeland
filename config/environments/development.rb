4adfdb1d28d706283e67aafbf3b1342e4d7a5672
Jason Lee <huacnlee@gmail.com>
2011-09-28 09:33:56 +0800

    Theme changed.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Homeland::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_view.debug_rjs             = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false

f4e5d007b55d367a1a7bf7e84c112ccca9dde2db
Jason Lee <huacnlee@gmail.com>
2011-10-26 13:48:13 -0400

    Bux fix in production, this version still have issue, can not upload image.

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ end
 module Homeland
   class Application < Rails::Application
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/uploaders)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.

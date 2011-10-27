0248cbf2a3600157eedd0049c60b34d8ce34786e
Jason Lee <huacnlee@gmail.com>
2011-10-26 21:12:16 -0400

    Fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.where(:login => params[:id]).first
-    @last_topics = @user.topics.recents.limit(20)          
+    @last_topics = @user.topics.recent.limit(20)          
     @last_replies = @user.replies.only(:topic_id).recents.limit(50).group
     set_seo_meta("#{@user.name}")
   end

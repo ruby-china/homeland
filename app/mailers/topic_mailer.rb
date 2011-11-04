c722f438c08b523f95dc9f9389d7b7d881f54f60
Jason Lee <huacnlee@gmail.com>
2011-11-05 00:42:30 +0800

    Fix mail notify.

--- app/mailers/topic_mailer.rb
+++ app/mailers/topic_mailer.rb
@@ -9,16 +9,18 @@ class TopicMailer < BaseMailer
   end
   
   def self.got_reply(reply)
-    # Thread.new {
+    Thread.new {
       @reply = reply
       @topic = Topic.find_by_id(@reply.topic_id)
       User.where(:_id.in => @topic.follower_ids).excludes(:_id => @reply.user_id).each do |u|
         # 跳过，如果用户不允许发邮件
-        # next if u.mail_new_answer == false
+        #next if u.mail_new_answer == false
         TopicMailer.new_reply(reply.id,u.email).deliver
       end
-      TopicMailer.new_reply(reply.id,@topic.user.email).deliver
-    # }
+      if @reply.user.email != @topic.user.email
+        TopicMailer.new_reply(reply.id,@topic.user.email).deliver
+      end
+    }
   end
   
   class Preview < MailView

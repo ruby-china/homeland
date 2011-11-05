2fcc73edc45972453b668d369ad5b5546730dffa
Daniel Lv <lgn21st@gmail.com>
2011-11-06 04:53:23 +0800

    Add Unicon confing file for just in case.

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/ruby/www/ruby-china/current"
+pid "/home/ruby/www/ruby-china/current/tmp/pids/unicorn.pid"
+stderr_path "/home/ruby/www/ruby-china/current/log/unicorn.log"
+stdout_path "/home/ruby/www/ruby-china/current/log/unicorn.log"
+
+listen "/tmp/unicorn.ruby-china.sock"
+worker_processes 2
+timeout 120

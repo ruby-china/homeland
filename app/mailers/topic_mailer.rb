3b427684102795977a558b66c3403c471153dadc
Jason Lee <huacnlee@gmail.com>
2011-11-06 14:11:44 +0800

    Fix mailer

--- app/mailers/topic_mailer.rb
+++ app/mailers/topic_mailer.rb
@@ -9,25 +9,25 @@ class TopicMailer < BaseMailer
   end
   
   def self.got_reply(reply)
-    Thread.new {
-      @reply = reply
-      @topic = Topic.find_by_id(@reply.topic_id)
-      emails = []
-      User.where(:_id.in => @topic.follower_ids).excludes(:_id => @reply.user_id).each do |u|
-        # 跳过，如果用户不允许发邮件
-        #next if u.mail_new_reply == false
-        emails << u.email
-      end
-      # 加上话题发起者
-      emails << @topic.user.email
-      # 去掉回复者
-      emails.delete(@reply.user.email)
-      # 唯一
-      emails.uniq!
-      emails.each do |email|
-        ::TopicMailer.new_reply(@reply.id,email).deliver
-      end
-    }
+    @reply = reply
+    @topic = Topic.find_by_id(@reply.topic_id)
+    emails = []
+    User.where(:_id.in => @topic.follower_ids).excludes(:_id => @reply.user_id).each do |u|
+      # 跳过，如果用户不允许发邮件
+      #next if u.mail_new_reply == false
+      emails << u.email
+    end
+    # 加上话题发起者
+    emails << @topic.user.email
+    # 去掉回复者
+    emails.delete(@reply.user.email)
+    # 唯一
+    emails.uniq!
+    emails.each do |email|
+      ::TopicMailer.new_reply(@reply.id,email).deliver
+    end
+  rescue => e
+    Rails.logger.error("TopicMailer.got_reply failed. #{e}")
   end
   
   class Preview < MailView

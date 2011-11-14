bc297e714e9b5bf612aa9a3baece3f654e713628
Jason Lee <huacnlee@gmail.com>
2011-11-14 13:26:24 +0800

    Fix AWS require

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -1,3 +1,4 @@
+require "aws/ses"
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
   :access_key_id     => Setting.aws_access_key_id,
   :secret_access_key => Setting.aws_secret_access_key

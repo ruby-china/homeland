e25aa94a697b168fc873537ffe4b079495ba9906
Jason Lee <huacnlee@gmail.com>
2011-11-19 22:12:28 +0800

    Update Unicorn config.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,24 @@
+module Rails
+  class <<self
+    def root
+      File.expand_path(__FILE__).split('/')[0..-3].join('/')
+    end
+  end
+end
 rails_env = ENV["RAILS_ENV"] || "production"
-working_directory "/home/ruby/www/ruby-china/current"
-pid "/home/ruby/www/ruby-china/current/tmp/pids/unicorn.pid"
-stderr_path "/home/ruby/www/ruby-china/current/log/unicorn.log"
-stdout_path "/home/ruby/www/ruby-china/current/log/unicorn.log"
+
+preload_app true
+working_directory Rails.root
+pid "#{Rails.root}/tmp/pids/unicorn.pid"
+stderr_path "#{Rails.root}/log/unicorn.log"
+stdout_path "#{Rails.root}/log/unicorn.log"
+
+listen 7000, :tcp_nopush => false
 
 listen "/tmp/unicorn.ruby-china.sock"
-worker_processes 2
+worker_processes 6
 timeout 120
+
+if GC.respond_to?(:copy_on_write_friendly=)
+  GC.copy_on_write_friendly = true
+end

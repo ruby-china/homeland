420779e6cb930a2b0ae94fa45948eb781595b743
Jason Lee <huacnlee@gmail.com>
2011-11-20 02:58:47 +0800

    Carrierwave GridFS config fix.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,10 @@
+mongoid_config = YAML.load_file("#{Rails.root}/config/mongoid.yml")[Rails.env]
 CarrierWave.configure do |config|
-  config.grid_fs_database = Mongoid.database.name
-  config.grid_fs_host = '127.0.0.1'
+  config.grid_fs_database = mongoid_config['database']
+  config.grid_fs_host = mongoid_config['host']
+  config.grid_fs_port = mongoid_config['port']
+  config.grid_fs_username = mongoid_config['username']
+  config.grid_fs_password = mongoid_config['password']
   config.storage = :grid_fs
   config.grid_fs_access_url = Setting.upload_url
 end

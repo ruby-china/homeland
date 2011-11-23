8b16270112f3ab8d2b455800f2bbdb05602f753f
xdite <xuite.joke@gmail.com>
2011-11-24 07:41:52 +0800

    tab to space

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User
   has_many :topics, :dependent => :destroy  
   has_many :notes
   has_many :replies, :dependent => :destroy
-	embeds_many :authorizations
+  embeds_many :authorizations
   has_many :posts
   has_many :notifications, :class_name => 'Notification::Base', :dependent => :delete
 
@@ -130,28 +130,28 @@ class User
     where("authorizations.provider" => hash['provider'], "authorizations.uid" => hash['uid']).first
   end
 
-	def self.create_from_hash(auth)  
-	  Rails.logger.debug(auth)
-		user = User.new
-		user.login = auth["user_info"]["nickname"] || auth["user_info"]["username"]
-		user.login.gsub!(/[^\w]/, '_')
-		user.login.slice!(0, 20)
-		if User.where(:login => user.login).count > 0 or user.login.blank?
-	    user.login = "u#{Time.now.to_i}"
-	  end
-		user.email = auth['user_info']['email']
-		user.location = auth['user_info']['location']
-		user.tagline =  auth["user_info"]["description"]
-		if not auth["user_info"]["urls"].blank?
-		  url_hash = auth["user_info"]["urls"].first
-		  user.website = url_hash.last
-	  end
-		if user.save(:validate => false)
-  		user.authorizations << Authorization.new(:provider => auth['provider'], :uid => auth['uid'])
-  		return user
-  	else
-  	  Rails.logger.warn("User.create_from_hash 失败，#{user.errors.inspect}")
-  	  return nil
-		end
+  def self.create_from_hash(auth)  
+    Rails.logger.debug(auth)
+    user = User.new
+    user.login = auth["user_info"]["nickname"] || auth["user_info"]["username"]
+    user.login.gsub!(/[^\w]/, '_')
+    user.login.slice!(0, 20)
+    if User.where(:login => user.login).count > 0 or user.login.blank?
+      user.login = "u#{Time.now.to_i}"
+    end
+    user.email = auth['user_info']['email']
+    user.location = auth['user_info']['location']
+    user.tagline =  auth["user_info"]["description"]
+    if not auth["user_info"]["urls"].blank?
+      url_hash = auth["user_info"]["urls"].first
+      user.website = url_hash.last
+    end
+    if user.save(:validate => false)
+      user.authorizations << Authorization.new(:provider => auth['provider'], :uid => auth['uid'])
+      return user
+    else
+      Rails.logger.warn("User.create_from_hash 失败，#{user.errors.inspect}")
+      return nil
+    end
   end  
 end

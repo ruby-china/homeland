fb8b77f013cef6f7e79e027fc2bdd6d7731d7f07
xdite <xuite.joke@gmail.com>
2011-11-24 14:51:58 +0800

    refactor omniauth_callbacks

--- app/models/user/omniauth_callbacks.rb
+++ app/models/user/omniauth_callbacks.rb
@@ -1,115 +1,51 @@
 # coding: utf-8  
 class User
   module OmniauthCallbacks
+    
+    ["github","google","twitter","douban"].each do |provider|
+      define_method "find_or_create_for_#{provider}" do |response|
+        uid = response["uid"]
+        data = response["info"]
+
+        if user = User.where("authorizations.provider" => provider , "authorizations.uid" => uid).first
+          user
+        elsif user = User.find_by_email(data["email"])
+          user.bind_service(response)
+          user
+        else 
+          user = User.new_from_provider_data(provider,uid,data)
+
+          if user.save(:validate => false)
+            user.authorizations << Authorization.new(:provider => provider, :uid => uid )
+            return user
+          else
+            Rails.logger.warn("User.create_from_hash 失败，#{user.errors.inspect}")
+            return nil
+          end
+        end
+        
+      end
+    end
      
-    def find_or_create_for_github(response)
-      provider = response["provider"]
-      uid = response["uid"]
-      data = response["info"]
+    
+    def new_from_provider_data(provider, uid, data)
+      user = User.new
+      user.email = data["email"]
+      user.email = "twitter+#{uid}@example.com" if provider == "twitter"
+      user.email = "douban+#{uid}@example.com" if provider == "douban"
       
-      if user = User.where("authorizations.provider" => provider , "authorizations.uid" => uid).first
-        user
-      elsif user = User.find_by_email(data["email"])
-        user.bind_service(response)
-        user
-      else # Create a user with a stub password. 
-        user = User.new(:email => data["email"],
-          :password => Devise.friendly_token[0,20],
-          :location => data["location"],
-          :tagline => data["description"],
-          :login => data["nickname"]
-        )
-        if user.save(:validate => false)
-          user.authorizations << Authorization.new(:provider => provider, :uid => uid )
-          return user
-        else
-          Rails.logger.warn("User.create_from_hash 失败，#{user.errors.inspect}")
-          return nil
-        end
+      user.login = data["nickname"]
+      user.login = data["name"] if provider == "google"
+      user.login.gsub!(/[^\w]/, "_")
+      if User.where(:login => user.login).count > 0 || user.login.blank?
+        user.login = "u#{Time.now.to_i}"
       end
-    end
-    
-    def find_or_create_for_twitter(response)
-      provider = response["provider"]
-      uid = response["uid"]
-      data = response["info"]
       
-      if user = User.where("authorizations.provider" => provider , "authorizations.uid" => uid).first
-        user
-      elsif user = User.find_by_email(data["email"])
-        user.bind_service(response)
-        user
-      else # Create a user with a stub password. 
-        user = User.new(:email => "twitter+#{uid}@example.com", 
-          :password => Devise.friendly_token[0,20],
-          :location => data["location"],
-          :tagline => data["description"],
-          :login => data["nickname"]
-        ) #
-        if user.save(:validate => false)
-          user.authorizations << Authorization.new(:provider => provider, :uid => uid )
-          return user
-        else
-          Rails.logger.warn("User.create_from_hash 失败，#{user.errors.inspect}")
-          return nil
-        end
-      end
-    end
-    
-    def find_or_create_for_douban(response)
-      provider = response["provider"]
-      uid = response["uid"]
-      data = response["info"]
+      user.password = Devise.friendly_token[0,20]
+      user.location = data["location"]
+      user.tagline = data["description"]
       
-      if user = User.where("authorizations.provider" => provider , "authorizations.uid" => uid).first
-        user
-      elsif user = User.find_by_email(data["email"])
-        user.bind_service(response)
-        user
-      else # Create a user with a stub password. 
-        user = User.new(:email => "douban+#{uid}@example.com",
-          :password => Devise.friendly_token[0,20],
-          :location => data["location"],
-          :tagline => data["description"],
-          :login => data["nickname"]
-        ) #
-        if user.save(:validate => false)
-          user.authorizations << Authorization.new(:provider => provider, :uid => uid )
-          return user
-        else
-          Rails.logger.warn("User.create_from_hash 失败，#{user.errors.inspect}")
-          return nil
-        end
-      end
+      return user
     end
-    
-    def find_or_create_for_google(response)
-      provider = response["provider"]
-      uid = response["uid"]
-      data = response["info"]
-      
-      if user = User.where("authorizations.provider" => provider , "authorizations.uid" => uid).first
-        user
-      elsif user = User.find_by_email(data["email"])
-        user.bind_service(response)
-        user
-      else # Create a user with a stub password. 
-        user = User.new(:email => data["email"],
-          :password => Devise.friendly_token[0,20],
-          :location => data["location"],
-          :tagline => data["description"],
-          :login => data["name"]
-        ) #
-        if user.save(:validate => false)
-          user.authorizations << Authorization.new(:provider => provider, :uid => uid )
-          return user
-        else
-          Rails.logger.warn("User.create_from_hash 失败，#{user.errors.inspect}")
-          return nil
-        end
-      end
-    end
-    
-    
   end
 end

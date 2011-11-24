04a6dfa0b29868b2d3a74a97f5e985e4e3e0359c
Fred Wu <ifredwu@gmail.com>
2011-11-24 18:02:57 +1100

    Almost always set local delivery_method to :test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ RubyChina::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :ses
+  config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log

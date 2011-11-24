d2661f884851ae7f34e6960944aa73170900ccd6
xdite <xuite.joke@gmail.com>
2011-11-24 17:37:19 +0800

    cpanel link refactor to link_to

--- app/controllers/cpanel/application_controller.rb
+++ app/controllers/cpanel/application_controller.rb
@@ -5,7 +5,7 @@ class Cpanel::ApplicationController < ApplicationController
   before_filter :require_admin
   
   def require_admin
-    if not Setting.admin_emails.index(current_user.email)
+    if not Setting.admin_emails.include?(current_user.email)
       render_404
     end
   end

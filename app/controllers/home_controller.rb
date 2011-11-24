517365a4b077e13ea672e784a78ba952cebdf073
xdite <xuite.joke@gmail.com>
2011-11-25 02:40:10 +0800

    remove legacy before_filter from home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 # coding: utf-8  
 class HomeController < ApplicationController
-  before_filter :require_no_user, :only => [:login, :login_create]
-  before_filter :require_user, :only => [:logout,:auth_unbind]
-  skip_before_filter :verify_authenticity_token, :only => [:auth_callback]
 
   def index
     unless current_user.blank?

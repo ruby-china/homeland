3ef7852fdcf077c0a2aa33d8fab67f163fd722de
xdite <xuite.joke@gmail.com>
2011-11-25 13:27:53 +0800

    把最近加入會員也拆入 Cell 把 hot_users 改名成 acitive_users

--- app/cells/users_cell.rb
+++ app/cells/users_cell.rb
@@ -1,9 +1,15 @@
 # coding: utf-8
 class UsersCell < BaseCell
   # 活跃会员
-  cache :hot_users, :expires_in => 1.days
-  def hot_users
-    @hot_users = User.hot.limit(20)
+  cache :active_users, :expires_in => 1.days
+  def active_users
+    @active_users = User.hot.limit(20)
     render 
+  end
+  
+  cache :active_users, :expires_in => 1.hour
+  def recent_join_users
+    @recent_join_users = User.recent.limit(20)
+    render
   end
 end

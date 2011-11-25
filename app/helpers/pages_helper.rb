68fe8ec45f2d3013059a91a75e08cff517e84158
xdite <xuite.joke@gmail.com>
2011-11-25 15:23:00 +0800

    upaddage helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,4 +3,16 @@ module PagesHelper
     return "" if page.blank?
     raw "<a href='#{page_path(page.slug)}' class='page'>#{h(page.title)}</a>"
   end
+  
+  def render_page_version(page)
+    page.version
+  end
+  
+  def render_page_updated_at(page)
+    timeago(page.updated_at)
+  end
+  
+  def render_edit_page_button(page)
+    link_to("编辑", edit_page_path(page), :class => "label success")
+  end
 end

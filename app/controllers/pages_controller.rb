a1ed51a94e9acb48b1db114690b8a25c2903a91f
xdite <xuite.joke@gmail.com>
2011-11-26 17:11:59 +0800

    [Feature] 如果找不到頁面就跳到新增 Wiki 頁

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,8 +20,12 @@ class PagesController < ApplicationController
   def show
     @page = Page.find_by_slug(params[:id])
     if !@page
-      render_404
-      return
+      if !current_user
+        render_404
+      else
+        redirect_to new_page_path(:title => params[:id]), :notice => "Page not Found, Please create a new page"
+        return
+      end
     end
     set_seo_meta("#{@page.title} - Wiki")
     drop_breadcrumb("查看 #{@page.title}")
@@ -29,8 +33,326 @@ class PagesController < ApplicationController
 
   def new
     @page = Page.new
-    set_seo_meta("创建 Wiki 页面")
-    drop_breadcrumb(" 创建 Wiki 页面")
+
+    @page.slug = params[:title]
+    set_seo_meta t("pages.new_wiki_page")
+    drop_breadcrumb t("pages.new_wiki_page")
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @page }
+    end
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+    set_seo_meta("修改 Wiki 页面")
+    drop_breadcrumb("编辑")
+  end
+
+  def create
+    @page = Page.new(params[:page])
+    @page.user_id = current_user.id
+    @page.version_enable = true
+
+    if @page.save
+      redirect_to page_path(@page.slug), notice: '页面创建成功。'
+    else
+      render action: "new"
+    end
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    params[:page][:version_enable] = true
+    params[:page][:user_id] = current_user.id
+    
+    if @page.update_attributes(params[:page])
+      redirect_to page_path(@page.slug), notice: '页面更新成功。'
+    else
+      render action: "edit"
+    end
+  end
+  
+  protected
+  
+  def set_menu_active
+    @current = @current = ['/wiki']
+  end
+  
+  def init_base_breadcrumb
+    drop_breadcrumb("Wiki", pages_path)
+  end
+
+  private
+    def check_lock
+      @page = Page.find(params[:id])
+      if @page.locked
+        if !current_user or !Setting.admin_emails.include?(current_user.email)
+          redirect_to page_path(@page.slug), alert: "抱歉，此页面已被锁定，只能管理员才能修改。"
+          return
+        end
+      end
+    end
+    
+    def check_permissions
+      if not current_user.wiki_editor?
+        render_403
+        return false
+      end
+      true
+    end
+end
+# coding: utf-8
+class PagesController < ApplicationController
+  before_filter :check_lock, :only => [:edit, :update]
+  before_filter :require_user, :only => [:new, :edit, :create, :update]
+  before_filter :check_permissions, :only => [:new, :edit, :create, :update]
+  before_filter :init_base_breadcrumb
+  before_filter :set_menu_active
+  
+  def index
+    set_seo_meta("Wiki")
+    drop_breadcrumb("索引")
+  end
+  
+  def recent
+    @pages = Page.recent.paginate(:page => params[:page], :per_page => 30)
+    set_seo_meta("Wiki 目录")
+    drop_breadcrumb("目录")
+  end
+
+  def show
+    @page = Page.find_by_slug(params[:id])
+    if !@page
+      if !current_user
+        render_404
+      else
+        redirect_to new_page_path(:title => params[:id]), :notice => "Page not Found, Please create a new page"
+        return
+      end
+    end
+    set_seo_meta("#{@page.title} - Wiki")
+    drop_breadcrumb("查看 #{@page.title}")
+  end
+
+  def new
+    @page = Page.new
+
+    @page.slug = params[:title]
+    set_seo_meta t("pages.new_wiki_page")
+    drop_breadcrumb t("pages.new_wiki_page")
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @page }
+    end
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+    set_seo_meta("修改 Wiki 页面")
+    drop_breadcrumb("编辑")
+  end
+
+  def create
+    @page = Page.new(params[:page])
+    @page.user_id = current_user.id
+    @page.version_enable = true
+
+    if @page.save
+      redirect_to page_path(@page.slug), notice: '页面创建成功。'
+    else
+      render action: "new"
+    end
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    params[:page][:version_enable] = true
+    params[:page][:user_id] = current_user.id
+    
+    if @page.update_attributes(params[:page])
+      redirect_to page_path(@page.slug), notice: '页面更新成功。'
+    else
+      render action: "edit"
+    end
+  end
+  
+  protected
+  
+  def set_menu_active
+    @current = @current = ['/wiki']
+  end
+  
+  def init_base_breadcrumb
+    drop_breadcrumb("Wiki", pages_path)
+  end
+
+  private
+    def check_lock
+      @page = Page.find(params[:id])
+      if @page.locked
+        if !current_user or !Setting.admin_emails.include?(current_user.email)
+          redirect_to page_path(@page.slug), alert: "抱歉，此页面已被锁定，只能管理员才能修改。"
+          return
+        end
+      end
+    end
+    
+    def check_permissions
+      if not current_user.wiki_editor?
+        render_403
+        return false
+      end
+      true
+    end
+end
+# coding: utf-8
+class PagesController < ApplicationController
+  before_filter :check_lock, :only => [:edit, :update]
+  before_filter :require_user, :only => [:new, :edit, :create, :update]
+  before_filter :check_permissions, :only => [:new, :edit, :create, :update]
+  before_filter :init_base_breadcrumb
+  before_filter :set_menu_active
+  
+  def index
+    set_seo_meta("Wiki")
+    drop_breadcrumb("索引")
+  end
+  
+  def recent
+    @pages = Page.recent.paginate(:page => params[:page], :per_page => 30)
+    set_seo_meta("Wiki 目录")
+    drop_breadcrumb("目录")
+  end
+
+  def show
+    @page = Page.find_by_slug(params[:id])
+    if !@page
+      if !current_user
+        render_404
+      else
+        redirect_to new_page_path(:title => params[:id]), :notice => "Page not Found, Please create a new page"
+        return
+      end
+    end
+    set_seo_meta("#{@page.title} - Wiki")
+    drop_breadcrumb("查看 #{@page.title}")
+  end
+
+  def new
+    @page = Page.new
+
+    @page.slug = params[:title]
+    set_seo_meta t("pages.new_wiki_page")
+    drop_breadcrumb t("pages.new_wiki_page")
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @page }
+    end
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+    set_seo_meta("修改 Wiki 页面")
+    drop_breadcrumb("编辑")
+  end
+
+  def create
+    @page = Page.new(params[:page])
+    @page.user_id = current_user.id
+    @page.version_enable = true
+
+    if @page.save
+      redirect_to page_path(@page.slug), notice: '页面创建成功。'
+    else
+      render action: "new"
+    end
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    params[:page][:version_enable] = true
+    params[:page][:user_id] = current_user.id
+    
+    if @page.update_attributes(params[:page])
+      redirect_to page_path(@page.slug), notice: '页面更新成功。'
+    else
+      render action: "edit"
+    end
+  end
+  
+  protected
+  
+  def set_menu_active
+    @current = @current = ['/wiki']
+  end
+  
+  def init_base_breadcrumb
+    drop_breadcrumb("Wiki", pages_path)
+  end
+
+  private
+    def check_lock
+      @page = Page.find(params[:id])
+      if @page.locked
+        if !current_user or !Setting.admin_emails.include?(current_user.email)
+          redirect_to page_path(@page.slug), alert: "抱歉，此页面已被锁定，只能管理员才能修改。"
+          return
+        end
+      end
+    end
+    
+    def check_permissions
+      if not current_user.wiki_editor?
+        render_403
+        return false
+      end
+      true
+    end
+end
+# coding: utf-8
+class PagesController < ApplicationController
+  before_filter :check_lock, :only => [:edit, :update]
+  before_filter :require_user, :only => [:new, :edit, :create, :update]
+  before_filter :check_permissions, :only => [:new, :edit, :create, :update]
+  before_filter :init_base_breadcrumb
+  before_filter :set_menu_active
+  
+  def index
+    set_seo_meta("Wiki")
+    drop_breadcrumb("索引")
+  end
+  
+  def recent
+    @pages = Page.recent.paginate(:page => params[:page], :per_page => 30)
+    set_seo_meta("Wiki 目录")
+    drop_breadcrumb("目录")
+  end
+
+  def show
+    @page = Page.find_by_slug(params[:id])
+    if !@page
+      if !current_user
+        render_404
+      else
+        redirect_to new_page_path(:title => params[:id]), :notice => "Page not Found, Please create a new page"
+        return
+      end
+    end
+    set_seo_meta("#{@page.title} - Wiki")
+    drop_breadcrumb("查看 #{@page.title}")
+  end
+
+  def new
+    @page = Page.new
+
+    @page.slug = params[:title]
+    set_seo_meta t("pages.new_wiki_page")
+    drop_breadcrumb t("pages.new_wiki_page")
+
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @page }

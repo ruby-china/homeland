715f71910ff3ee43c91f5a6e90e3cb43232fed52
xdite <xuite.joke@gmail.com>
2011-12-01 11:33:21 +0800

    將 topics 跟 wiki 搜尋結果全切開

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,39 @@
 # coding: utf-8
 class SearchController < ApplicationController
+
+  before_filter :validate_search_key
+
   def index
-    
-    @q = params[:q].gsub(/\\|\'|-|\/|\.|\?/, "") if params[:q].present?
 
     if @q.present?
       @topics = Topic.search(@q).paginate(:page => params[:page], :per_page => 20)
-      @pages = Page.search(@q).paginate(:page => params[:page], :per_page => 20)
     end
 
+    render :action => :topics
+  end
+
+  def topics
+    
+    if @q.present?
+      @topics = Topic.search(@q).paginate(:page => params[:page], :per_page => 20)
+    end
+    
     set_seo_meta("#{t("common.search")}: #{@q}")
     drop_breadcrumb("#{t("common.search")}: #{@q}")
-
-    render :stream => true
   end
+
+  def wiki
+    if @q.present?
+      @pages = Page.search(@q).paginate(:page => params[:page], :per_page => 20)
+    end
+    
+    set_seo_meta("#{t("common.search")}: #{@q}")
+    drop_breadcrumb("#{t("common.search")}: #{@q}")
+  end
+
+  protected
+
+    def validate_search_key
+      @q = params[:q].gsub(/\\|\'|-|\/|\.|\?/, "") if params[:q].present?
+    end
 end

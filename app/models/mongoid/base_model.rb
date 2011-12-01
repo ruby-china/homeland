ce37693e1f417445b5d6291f7a0499edefea700e
James Chen <ashchan@gmail.com>
2011-12-02 00:28:03 +0900

    a little refactor on Mongoid::BaseModel; also added tests for it

--- app/models/mongoid/base_model.rb
+++ app/models/mongoid/base_model.rb
@@ -1,16 +1,14 @@
 # coding: utf-8
 # 基本 Model，加入一些通用功能
 module Mongoid
-  module BaseModel      
+  module BaseModel
     extend ActiveSupport::Concern
-    
-    def self.included(base)
-      base.instance_eval do
-        scope :recent, desc(:_id)
-        scope :exclude_ids, Proc.new { |ids| where(:_id.nin => ids.collect { |id| id.to_i }) }
-      end
+
+    included do
+      scope :recent, desc(:_id)
+      scope :exclude_ids, Proc.new { |ids| where(:_id.nin => ids.map(&:to_i)) }
     end
-    
+
     module ClassMethods
       # like ActiveRecord find_by_id
       def find_by_id(id)

168a3a449551cc38c22fb25f98b392180630c982
Winfield <winfield301@gmail.com>
2011-12-02 13:44:10 +0800

    fix the test

--- spec/models/cpanel/node_spec.rb
+++ spec/models/cpanel/node_spec.rb
@@ -5,6 +5,8 @@ describe Node do
   describe 'Validates' do   
     it 'should fail saving without specifing a section' do
       node = Node.new
+      node.name = "Cersei"
+      node.summary = "the Queue"
       node.save == false
     end
   end

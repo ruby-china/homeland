7f86f78b81d0795d782a28464c7bdb5d61da771b
huangzg <huangzg@csdn.net>
2011-12-02 12:50:45 +0800

    fix typo.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,6 +30,7 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+  config.render_views
 
   config.include Devise::TestHelpers, :type => :controller
   config.after do

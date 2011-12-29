bbe4b09cfecaa07992776e5fd01ab4f700b034ca
Jason Lee <huacnlee@gmail.com>
2011-12-29 15:54:16 +0800

    Add for Mobile support.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -70,4 +70,14 @@ module ApplicationHelper
     Setting.facebook_enable
   end
   
+  MOBILE_USER_AGENTS =  'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
+                        'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
+                        'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
+                        'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
+                        'webos|amoi|novarra|cdm|alcatel|pocket|iphone|mobileexplorer|mobile'
+  def mobile?
+    agent_str = request.user_agent.to_s.downcase
+    return false if agent_str =~ /ipad/
+    agent_str =~ Regexp.new(MOBILE_USER_AGENTS)
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ RubyChina::Application.configure do
   config.assets.css_compressor = :scss
   config.assets.precompile += %w(application.css application.js topics.css topics.js window.css front.css cpanel.css search.css
   users.css posts.css posts.js pages.css pages.js notifications.js notifications.css sites.css sites.js 
-  gfdynamicfeedcontrol.css gfdynamicfeedcontrol.js)
+  gfdynamicfeedcontrol.css gfdynamicfeedcontrol.js mobile.css)
   
   
 end

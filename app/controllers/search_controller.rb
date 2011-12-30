163eef307d595ae64953b7c39037890866ec78b9
cricy <xifeng@taobao.com>
2011-12-30 11:10:59 +0800

    Sunspot fix on Mongoid 2.3.x (search method was used.)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
   def index
     @keywords = Segment.split(params[:q])
     search_text = @keywords.join(" ")
-    @search = Topic.search do
+    @search = Sunspot.search(Topic) do
       keywords search_text, :highlight => true
       paginate :page => params[:page], :per_page => 20
       order_by :replied_at, :desc

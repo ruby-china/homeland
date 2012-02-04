634a93fd184346189abdab51c43a0a275547a8fc
Jason Lee <huacnlee@gmail.com>
2012-02-04 18:32:55 +0800

    Lock user login field modify for cache feature.

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,6 +1,5 @@
 class AccountController < Devise::RegistrationsController
   def destroy
-    logger.debug { "---------------- AccountController" }
     resource.soft_delete
     sign_out_and_redirect("/login")
     set_flash_message :notice, :destroyed

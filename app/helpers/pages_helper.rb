437b9f8aa9c8d7e768a87db9a476f8a96adfd30d
Forrest Ye <afu@forresty.com>
2012-02-10 03:02:42 +0800

    clean up pages helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,18 +1,18 @@
-# coding: utf-8  
+# coding: utf-8
 module PagesHelper
   def page_title_tag(page)
     return "" if page.blank?
     link_to(page.title, page_path(page.slug), :class => "page")
   end
-  
+
   def render_page_version(page)
     page.version
   end
-  
+
   def render_page_updated_at(page)
     timeago(page.updated_at)
   end
-  
+
   def render_edit_page_button(page)
     link_to("", edit_page_path(page), :class => "icon small_edit")
   end

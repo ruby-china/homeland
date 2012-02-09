a7fe6b682f0848e217c209eeb184e45ee4f14913
Forrest Ye <afu@forresty.com>
2012-02-10 04:30:37 +0800

    spec and clean up omniauth_callbacks module

--- app/models/user/omniauth_callbacks.rb
+++ app/models/user/omniauth_callbacks.rb
@@ -1,7 +1,6 @@
-# coding: utf-8  
+# coding: utf-8
 class User
   module OmniauthCallbacks
-    
     ["github","google","twitter","douban"].each do |provider|
       define_method "find_or_create_for_#{provider}" do |response|
         uid = response["uid"]
@@ -12,8 +11,8 @@ class User
         elsif user = User.find_by_email(data["email"])
           user.bind_service(response)
           user
-        else 
-          user = User.new_from_provider_data(provider,uid,data)
+        else
+          user = User.new_from_provider_data(provider, uid, data)
 
           if user.save(:validate => false)
             user.authorizations << Authorization.new(:provider => provider, :uid => uid )
@@ -23,29 +22,27 @@ class User
             return nil
           end
         end
-        
       end
     end
-     
-    
+
     def new_from_provider_data(provider, uid, data)
-      user = User.new
-      user.email = data["email"]
-      user.email = "twitter+#{uid}@example.com" if provider == "twitter"
-      user.email = "douban+#{uid}@example.com" if provider == "douban"
-      
-      user.login = data["nickname"]
-      user.login = data["name"] if provider == "google"
-      user.login.gsub!(/[^\w]/, "_")
-      if User.where(:login => user.login).count > 0 || user.login.blank?
-        user.login = "u#{Time.now.to_i}"
+      User.new do |user|
+        user.email = data["email"]
+        user.email = "twitter+#{uid}@example.com" if provider == "twitter"
+        user.email = "douban+#{uid}@example.com" if provider == "douban"
+
+        user.login = data["nickname"]
+        user.login = data["name"] if provider == "google"
+        user.login.gsub!(/[^\w]/, "_")
+
+        if User.where(:login => user.login).count > 0 || user.login.blank?
+          user.login = "u#{Time.now.to_i}" # TODO: possibly duplicated user login here. What should we do?
+        end
+
+        user.password = Devise.friendly_token[0, 20]
+        user.location = data["location"]
+        user.tagline = data["description"]
       end
-      
-      user.password = Devise.friendly_token[0,20]
-      user.location = data["location"]
-      user.tagline = data["description"]
-      
-      return user
     end
   end
 end

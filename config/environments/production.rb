59f66971e4b04906bfd96249895baa582abaa1f7
Jason Lee <huacnlee@gmail.com>
2012-02-21 22:48:17 +0800

    Fix mail sender, still use aws-ses to send, need confirm there have right info on production environment, Devise use resque-mailer has a bug, can not use. But the others mail send use background send.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ RubyChina::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :ses
 
   # Enable threaded mode
   # config.threadsafe!

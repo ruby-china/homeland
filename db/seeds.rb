995b02692c23d88da93a0cf65a37f441927a74ac
Jason Lee <huacnlee@gmail.com>
2012-03-01 17:45:06 +0800

    Revert seed code.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
+
 # 默认配置项
 # 如需新增设置项，请在这里初始化默认值，然后到后台修改
-
 # 首页
 # SiteConfig.index_html
 SiteConfig.save_default("index_html",<<-eos
@@ -68,3 +68,34 @@ SiteConfig.save_default("site_index_html",<<-eos
 下面列出了基于 Ruby 语言开发的网站。如果你知道还有不在此列表的，请帮忙补充。
 eos
 )
+
+
+# ========================= init Section, Node =========================
+
+# s1 = Section.create(:name => "Ruby")
+# Node.create(:name => "Ruby",:summary => "...", :section_id => s1.id)
+# Node.create(:name => "Ruby on Rails",:summary => "...", :section_id => s1.id)
+# Node.create(:name => "Gem",:summary => "...", :section_id => s1.id)
+# s2 = Section.create(:name => "Web Development")
+# Node.create(:name => "Python",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Javascript",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "CoffeeScript",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "HAML",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "SASS",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "MongoDB",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Redis",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Git",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "MySQL",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Hadoop",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Google",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Java",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Tornado",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Linux",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Nginx",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Apache",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Cloud",:summary => "...", :section_id => s2.id)
+# s6 = Section.create(:name => "Ruby China")
+# Node.create(:name => "公告",:summary => "...", :section_id => s6.id)
+# Node.create(:name => "反馈",:summary => "...", :section_id => s6.id)
+# Node.create(:name => "开发",:summary => "...", :section_id => s6.id)
+

e5241e8ec71b310905ccb38b3d903c3a2bf93156
Jason Lee <huacnlee@gmail.com>
2012-03-01 09:44:30 +0800

    Merged pull request #218

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,9 @@ class ApplicationController < ActionController::Base
   def render_optional_error_file(status_code)
     status = status_code.to_s
     if ["404","403", "422", "500"].include?(status)
-      render :template => "/errors/#{status}.html.erb", :status => status, :layout => "application"
+      render :template => "/errors/#{status}", :format => [:html], :handler => [:erb], :status => status, :layout => "application"
     else
-      render :template => "/errors/unknown.html.erb", :status => status, :layout => "application"
+      render :template => "/errors/unknown", :format => [:html], :handler => [:erb], :status => status, :layout => "application"
     end
   end
 

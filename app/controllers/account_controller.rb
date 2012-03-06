9dd43546fc9fe862d8705b4f86db5e97890b678d
Jason Lee <huacnlee@gmail.com>
2012-03-06 16:31:08 +0800

    Fix issue #227, Can not sign up a new User.

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,4 +1,25 @@
 class AccountController < Devise::RegistrationsController
+
+  # POST /resource
+  def create
+    build_resource
+    resource.login = params[resource_name][:login]
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_in(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
   def destroy
     resource.soft_delete
     sign_out_and_redirect("/login")
--- app/models/user.rb
+++ app/models/user.rb
@@ -55,7 +55,7 @@ class User
   end
 
   attr_accessor :password_confirmation
-  attr_accessible :name, :email, :location, :bio, :website, :github, :tagline, :avatar
+  attr_accessible :name, :email, :location, :bio, :website, :github, :tagline, :avatar, :password, :password_confirmation
 
   validates :login, :format => {:with => /\A\w+\z/, :message => '只允许数字、大小写字母和下划线'}, :length => {:in => 3..20}, :presence => true, :uniqueness => {:case_sensitive => false}
 

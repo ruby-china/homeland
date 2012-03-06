a1d195b1b9c4d26d0d1419d9c91b0b498ccec621
James Chen <ashchan@gmail.com>
2012-03-07 08:58:42 +0900

    use fiction in test, real name is uncool.

--- spec/requests/signup_login_spec.rb
+++ spec/requests/signup_login_spec.rb
@@ -6,21 +6,21 @@ describe "sign up and login" do
   it "let user sign up and login to the site" do
     visit '/'
     click_link '注册'
-    fill_in '用户名', :with => 'ashchan'
-    fill_in 'Email', :with => 'ashchan@gmail.com'
+    fill_in '用户名', :with => 'rubyist'
+    fill_in 'Email', :with => 'rubyist@ruby-china.org'
     fill_in '密码', :with => 'coolguy'
     fill_in '确认密码', :with => 'coolguy'
     click_button '提交注册信息'
     page.should have_content('活跃帖子')
     within("#userbar") do
-      click_on 'ashchan'
+      click_on 'rubyist'
     end
 
     click_link '退出'
     page.should have_content('退出成功.')
 
     click_link '登录'
-    fill_in '用户名', :with => 'ashchan'
+    fill_in '用户名', :with => 'rubyist'
     fill_in '密码', :with => 'coolguy'
     click_button '登录'
     page.should have_content('活跃帖子')

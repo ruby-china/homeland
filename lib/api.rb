7963ad75c242b4562ce94d46602d6d68aff3b2d7
James Chen <ashchan@gmail.com>
2012-03-06 10:50:17 +0900

    topics list api accepts a size param

--- lib/api.rb
+++ lib/api.rb
@@ -9,8 +9,13 @@ module RubyChina
     helpers APIHelper::Topic
 
     resource :topics do
+      # Get active topics list
+      # params[:size] could be specified to limit the results
+      # params[:size]: default is 15, max is 100
       get do
-        @topics = Topic.last_actived.limit(15).includes(:user)
+        @topics = Topic.last_actived
+          .limit(page_size)
+          .includes(:user)
         present @topics, :with => APIEntities::Topic
       end
     end
---
+++ spec/api/helpers/topic.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe RubyChina::APIHelper::Topic do
+  before { extend RubyChina::APIHelper::Topic }
+
+  it "should use size that is valid" do
+    stub_params(:size => 10)
+    page_size.should == 10
+  end
+
+  it "should use max size if page size provided greater than that" do
+    stub_params(:size => max_page_size + 1)
+    page_size.should == max_page_size
+  end
+
+  it "should use default size if page size provided is not valid" do
+    stub_params(:size => 'a')
+    page_size.should == default_page_size
+  end
+
+  private
+  def stub_params(p = {})
+    self.stub!(:params).and_return(p)
+  end
+end

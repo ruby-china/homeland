6cb642cf7eb032881e6133075e90acf18bc88014
Jason Lee <huacnlee@gmail.com>
2012-03-08 12:47:47 +0800

    autoreload lib path files.

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module RubyChina
   class Application < Rails::Application
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/uploaders)
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.

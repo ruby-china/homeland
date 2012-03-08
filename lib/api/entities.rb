23677a082c998f5c2c79ebb1d12e8f5e61f17e93
Jason Lee <huacnlee@gmail.com>
2012-03-08 12:48:10 +0800

    Add last_replied_user_login to Topics api.

--- lib/api/entities.rb
+++ lib/api/entities.rb
@@ -8,7 +8,7 @@ module RubyChina
     end
 
     class Topic < Grape::Entity
-      expose :_id, :title, :body, :body_html, :created_at, :updated_at, :replied_at, :replies_count, :node_name, :node_id
+      expose :_id, :title, :body, :body_html, :created_at, :updated_at, :replied_at, :replies_count, :node_name, :node_id, :last_reply_user_login
       #FIXME: eager loading doesn't seem to be working here
       expose :user, :using => APIEntities::User
     end

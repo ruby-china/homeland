7714d33deaeae8c4df7d0b0df7282c79c113831c
James Chen <ashchan@gmail.com>
2012-03-17 17:30:30 +0900

    implemented topics by node api. closed #243
    also added simple api example in comments

--- lib/api.rb
+++ lib/api.rb
@@ -16,8 +16,23 @@ module RubyChina
       # Get active topics list
       # params[:size] could be specified to limit the results
       # params[:size]: default is 15, max is 100
+      # Example
+      #   /api/topics/index.json?size=30
       get do
         @topics = Topic.last_actived
+          .limit(page_size)
+          .includes(:user)
+        present @topics, :with => APIEntities::Topic
+      end
+
+      # Get active topics of the specified node
+      # params[:id]: node id
+      # other params are same to those of topics#index
+      # Example
+      #   /api/topics/node/1.json?size=30
+      get "node/:id" do
+        @node = Node.find(params[:id])
+        @topics = @node.topics.last_actived
           .limit(page_size)
           .includes(:user)
         present @topics, :with => APIEntities::Topic
@@ -37,6 +52,9 @@ module RubyChina
         #TODO error handling
       end
 
+      # Get topic detail
+      # Example
+      #   /api/topics/1.json
       get ":id" do
         @topic = Topic.includes(:replies => [:user]).where(:_id => params[:id]).first
         present @topic, :with => APIEntities::Topic
@@ -45,6 +63,8 @@ module RubyChina
 
     resource :nodes do
       # Get a list of all nodes
+      # Example
+      #   /api/nodes.json
       get do
         present Node.all, :with => APIEntities::Node
       end

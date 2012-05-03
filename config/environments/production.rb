06d99173b50adb8aa82c6f87ebb246ee3d1ffd3c
Jason Lee <huacnlee@gmail.com>
2012-05-03 09:38:06 +0800

    Logger split by weekly.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,11 +19,9 @@ RubyChina::Application.configure do
   # If you have no front-end server that supports something like X-Sendfile,
   # just comment this out and Rails will serve the files
 
-  # See everything in the log (default is :info)
-  # config.log_level = :debug
-
   # Use a different logger for distributed setups
-  # config.logger = SyslogLogger.new
+  config.logger = Logger.new("#{Rails.root}/log/#{Rails.env}.log", 'weekly')
+  config.logger.level = Logger::INFO
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store

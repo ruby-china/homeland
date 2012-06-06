1ee46a8f07beb739484d07eb744abfaf52c703f0
Jason Lee <huacnlee@gmail.com>
2012-06-06 17:30:42 +0800

    Remove useless Like files.

--- db/migrate/20120606080644_move_likes_data_to_topic_liked_user_ids_field.rb
+++
@@ -1,9 +0,0 @@
-class MoveLikesDataToTopicLikedUserIdsField < Mongoid::Migration
-  def self.up
-    Topic.where(:likes_count.gt => 0).each do |topic|
-      topic.set(:liked_user_ids, Like.where(:likeable_type => "Topic", :likeable_id => topic.id).collect { |like| like.user_id })
-      print "."
-    end
-    Like.collection.drop
-  end
-end
--- db/migrate/20120606084229_remove_likes_count_from_user_collection.rb
+++
@@ -1,8 +0,0 @@
-class RemoveLikesCountFromUserCollection < Mongoid::Migration
-  def self.up
-    User.where(:likes_count.gt => 0).each do |user|
-      user.unset(:likes_count)
-      print "."
-    end
-  end
-end

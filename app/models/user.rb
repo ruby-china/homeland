7ddae0eb0acc754f2237db1d0949a666f44723cb
Jason Lee <huacnlee@gmail.com>
2012-06-19 12:34:40 +0800

    Fix testing bugs.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,27 @@ class User
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
+  field :email,              :type => String, :default => ""
+  field :encrypted_password, :type => String, :default => ""
+
+  validates_presence_of :email
+
+  ## Recoverable
+  field :reset_password_token,   :type => String
+  field :reset_password_sent_at, :type => Time
+
+  ## Rememberable
+  field :remember_created_at, :type => Time
+
+  ## Trackable
+  field :sign_in_count,      :type => Integer, :default => 0
+  field :current_sign_in_at, :type => Time
+  field :last_sign_in_at,    :type => Time
+  field :current_sign_in_ip, :type => String
+  field :last_sign_in_ip,    :type => String
+
   field :login
   field :name
-  field :email
   field :location
   field :location_id, :type => Integer
   field :bio

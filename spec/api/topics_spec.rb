68dd4ac70b8cdaf0e2e944d0a3b9a6bdb0155e65
Jason Lee <huacnlee@gmail.com>
2012-07-17 00:03:26 +0800

    Mongoid 3.0.1, and fix test.

--- spec/api/topics_spec.rb
+++ spec/api/topics_spec.rb
@@ -36,8 +36,8 @@ describe RubyChina::API, "topics" do
   describe "GET /api/topics/:id.json" do
     it "should get topic detail with list of replies" do
       t = Factory(:topic, :title => "i want to know")
-      r = Factory(:reply, :topic_id => t._id, :body => "let me tell")
-      get "/api/topics/#{t._id}.json"
+      r = Factory(:reply, :topic_id => t.id, :body => "let me tell")
+      get "/api/topics/#{t.id}.json"
       response.status.should == 200
       json = JSON.parse(response.body)
       json["title"].should == "i want to know"

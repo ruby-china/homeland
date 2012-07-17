5e3abbbb9ff3129cf684e76842b2a5cbdd09ac15
Jason Lee <huacnlee@gmail.com>
2012-07-17 10:31:03 +0800

    OAuth unbind bug fix. #69

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
       return
     end
 
-    current_user.authorizations.destroy_all(:conditions => {:provider => provider})
+    current_user.authorizations.destroy_all({ :provider => provider })
     redirect_to edit_user_registration_path, :flash => {:warring => t("users.unbind_success", :provider => provider.titleize )}
   end
 

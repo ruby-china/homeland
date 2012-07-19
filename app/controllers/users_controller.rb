371d3c9ba9c28325f7d243632a20eab62d8e4e65
Jason Lee <huacnlee@gmail.com>
2012-07-20 01:42:29 +0800

    Keeping lower case letters for User profile url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,12 @@ class UsersController < ApplicationController
 
   protected
   def find_user
+    # 处理 login 有大写字母的情况
+    if params[:id] != params[:id].downcase
+      redirect_to request.path.downcase, :status => 301
+      return
+    end
+
     @user = User.where(:login => /^#{params[:id]}$/i).first
     render_404 if @user.nil?
   end

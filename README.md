6e74ce8b8740e18df2788881a4f2ffe8672e47bc
Jason Lee <huacnlee@gmail.com>
2012-08-03 18:27:49 +0800

    Add setup.rb to quick install ruby-china

--- README.md
+++ README.md
@@ -1,32 +1,41 @@
-## Ruby-China
+## Ruby China
 
 This is the source code of [Ruby China](http://ruby-china.org) website.
 
 [![Build
 Status](https://secure.travis-ci.org/ruby-china/ruby-china.png?branch=master&.png)](http://travis-ci.org/ruby-china/ruby-china)
 
+## Requirements
+
+* Ruby 1.9.2+
+* Memcached 1.4+
+* Redis 2.2+
+* Python 2.4+ and [Pygments](http://pygments.org)
+* MongoDb 2.0+
+
 ## Install
 
-* You need *Ruby 1.9.2+*, *Rubygems* and *Rails 3.2+* first.
-* Install and start *Redis*, *MongoDB*, *memcached*, *Python*, *Pygments*
-
-and run:
-
 ```bash
-easy_install pygments # 或者 pip install pygments
-cp config/config.yml.default config/config.yml
-cp config/mongoid.yml.default config/mongoid.yml
-cp config/redis.yml.default config/redis.yml
-cp config/thin.yml.default config/thin.yml
-bundle install
-bundle exec rake db:migrate
-bundle exec rake db:seed
-bundle exec sidekiq -c config/sidekiq.yml
-bundle exec rake sunspot:solr:start
+git clone git://github.com/ruby-china/ruby-china.git
+cd ruby-china
+# install pygments
+easy_install pygments
+# setup ruby-china
+ruby setup.rb
+# run server
 rails s
 ```
 
-or you can just this issue
+## Start Sidekiq and Solr service
+
+```bash
+# Sidekiq
+bundle exec sidekiq -c config/sidekiq.yml
+# Solr
+bundle exec rake sunspot:solr:start
+```
+
+## Testing
 
 ```bash
 bundle exec rspec spec
@@ -34,20 +43,9 @@ bundle exec rspec spec
 
 to prepare all the config files and start essential services.
 
-## Deploy
-
-```bash
-$ cap deploy
-$ cap production remote_rake:invoke task=db:setup
-```
-
-# Apply Google JSAPI
+## Apply Google JSAPI
 
 * http://code.google.com/intl/zh-CN/apis/loader/signup.html
-
-## Memcached
-
-Dalli requires memcached 1.4.x +
 
 ## Contributors
 
---
+++ setup.rb
@@ -0,0 +1,113 @@
+#!/usr/bin/env ruby
+ROW_SIZE = 80
+
+class String
+  COLORS = {
+    :red => "\033[31m",
+    :green => "\033[32m",
+    :yellow => "\033[33m",
+    :blue => "\033[34m"
+  }
+  def colorize(color)
+    "#{COLORS[color]}#{self}\033[0m"
+  end
+end
+
+def puts_section(info, &block)
+  puts info
+  puts "-"*ROW_SIZE
+  yield block
+  puts "-"*ROW_SIZE
+  puts ""
+end
+
+def puts_line(info, &block)
+  print info
+  rsize = ROW_SIZE - info.length
+  success = yield block
+  if success == false
+    puts "[Failed]".rjust(rsize).colorize(:red)
+  else
+    puts "[Done]".rjust(rsize).colorize(:green)
+  end
+end
+
+def puts_line_with_yn(info, &block)
+  print info
+  rsize = ROW_SIZE - info.length
+  success = yield block
+  if success == false
+    puts "[No]".rjust(rsize).colorize(:red)
+  else
+    puts "[Yes]".rjust(rsize).colorize(:green)
+  end
+end
+
+def replace_file(file_name, from, to)
+  File.open(file_name, "r+") do |f|
+    out = ""
+    f.each do |line|
+        out << line.gsub(from, to)
+    end
+    f.pos = 0
+    f.print out
+    f.truncate(f.pos)
+  end
+end
+
+puts "Now will installing Ruby China..."
+puts "="*ROW_SIZE
+puts ""
+
+puts_section "Checking Packages Depending..." do
+  pkg_exist = true
+  [["bundle","Bundler"],["java","Java 1.6+"],["python","Python 2.5+"],["pygmentize","Pygments 1.5+"],["mongod","MongoDB 2.0+"],["redis-server","Redis 2.0+"],["memcached","Memcached 1.4+"]].each do |item|
+    puts_line_with_yn item[1] do
+      if `which #{item[0]}` == ""
+        pkg_exist = false
+        false
+      else
+        true
+      end
+    end
+  end
+
+  exit(0) if pkg_exist == false
+end
+
+# Config files
+puts_section "Configure" do
+  %w(config mongoid redis thin).each do |fname|
+    `cp config/#{fname}.yml.default config/#{fname}.yml`
+  end
+
+  print "You MongoDB host (default: 127.0.0.1:27017):"
+  host = gets.strip
+  host = "127.0.0.1:27017" if host == ""
+  replace_file('config/mongoid.yml','SETUP_DEVELOPMENT_HOST',host)
+
+  print "You Redis host (default: 127.0.0.1:6379):"
+  host = gets.strip
+  host = "127.0.0.1:27017" if host == ""
+  replace_file('config/redis.yml','SETUP_REDIS_HOST',host.split(":")[0])
+  replace_file('config/redis.yml','SETUP_REDIS_PORT',host.split(":")[1])
+end
+
+puts_line "Install gems..." do
+  # `bundle install`
+end
+
+puts_line "Seed default datas..." do
+  # `bundle exec rake db:seed`
+end
+
+print "Do you want start Solr? (Y/N):"
+if gets.strip.downcase == "y"
+  puts "Starting Solr..."
+  `bundle exec rake sunspot:solr:start`
+  puts ""
+  puts "Now you can run: " + "rails s".colorize(:blue) + " to start application."
+else
+  puts ""
+  puts "Ruby China Install successed."
+end

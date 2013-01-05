ea3e092ee3ccfce9a34d2ed2a5486f11e6048f13
Jason Lee <huacnlee@gmail.com>
2013-01-05 17:35:52 +0800

    RSpec 里面强制用 test 环境，以免误操作.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@
 #end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV["RAILS_ENV"] = 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'

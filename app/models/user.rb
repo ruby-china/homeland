ecc0b6f6e78cc7eaefdf925d5aef24bb8777dc43
Tommy xiao <xiaods@gmail.com>
2013-03-24 23:00:36 +0800

    FIX: user reply limit test failing fix
    ruby-china.org/topics/9650

--- app/models/user.rb
+++ app/models/user.rb
@@ -145,7 +145,7 @@ class User
   # 是否能发帖
   def newbie?
     return false if self.verified == true
-    self.created_at > 1.week.ago || self.replies_count < 20
+    self.created_at > 1.week.ago || self.replies_count < 10
   end
 
   def has_role?(role)

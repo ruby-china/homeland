48a28ac1a2fff325cd0849ddbfdd6ee57fa3791f
Jason Lee <huacnlee@gmail.com>
2013-08-05 11:45:11 +0800

    -

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ require "sidekiq/capistrano"
 
 require "rvm/capistrano"
 default_run_options[:pty] = true
-set :rvm_ruby_string, 'ruby-2.0.0-p0'
+set :rvm_ruby_string, 'ruby-2.0.0-p247'
 set :rvm_type, :user
 
 set :application, "ruby-china"
@@ -12,18 +12,14 @@ set :repository,  "git://github.com/ruby-china/ruby-china.git"
 set :branch, "master"
 set :scm, :git
 set :user, "ruby"
-if ENV["DEPLOY"] == "pre"
-  set :deploy_to, "/data/www/#{application}-pre"
-else
-  set :deploy_to, "/data/www/#{application}"
-end
+set :deploy_to, "/data/www/#{application}"
 set :runner, "ruby"
 # set :deploy_via, :remote_cache
 # set :git_shallow_clone, 1
 
-role :web, "s2.ruby-china.org"                          # Your HTTP server, Apache/etc
-role :app, "s2.ruby-china.org"                          # This may be the same as your `Web` server
-role :db,  "s2.ruby-china.org", :primary => true # This is where Rails migrations will run
+role :web, "ruby-china.org"                          # Your HTTP server, Apache/etc
+role :app, "ruby-china.org"                          # This may be the same as your `Web` server
+role :db,  "ruby-china.org", :primary => true # This is where Rails migrations will run
 
 # unicorn.rb 路径
 set :unicorn_path, "#{deploy_to}/current/config/unicorn.rb"
@@ -90,4 +86,4 @@ task :mongoid_migrate_database, :roles => :web do
   run "cd #{deploy_to}/current/; RAILS_ENV=production bundle exec rake db:migrate"
 end
 
-after "deploy:finalize_update","deploy:symlink", :init_shared_path, :link_shared_files, :compile_assets, :sync_assets_to_cdn, :mongoid_migrate_database
+after "deploy:finalize_update","deploy:symlink", :init_shared_path, :link_shared_files#, :compile_assets, :sync_assets_to_cdn, :mongoid_migrate_database

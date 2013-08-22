80416815b5d3ab933403643fe2c4b2d67f98854b
Jason Lee <huacnlee@gmail.com>
2013-08-22 12:04:28 +0800

    去掉 Ruby Taiwan 的 gadget，那边已经基本没有更新了;

--- config/application.rb
+++ config/application.rb
@@ -46,7 +46,7 @@ module RubyChina
     
     config.assets.precompile += %w(application.css app.js topics.css topics.js window.css front.css cpanel.css
         users.css pages.css pages.js notes.css notes.js 
-        gfdynamicfeedcontrol.css gfdynamicfeedcontrol.js mobile.css home.css social-share-button.png social-share-button@2x.png)
+        mobile.css home.css)
   end
 end
 

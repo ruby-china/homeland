849d24a74e1e8d9c75e32f52f335339b843760be
Jason Lee <huacnlee@gmail.com>
2013-10-31 00:28:48 +0800

    4 workers with Puma.

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@ APP_ROOT = '/home/ruby/www/ruby-china/current'
 pidfile "#{APP_ROOT}/tmp/pids/puma.pid"
 state_path "#{APP_ROOT}/tmp/pids/puma.state"
 daemonize true
-workers 2
+workers 4
 threads 8,32
 preload_app!
 

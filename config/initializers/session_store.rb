e23ebaf93b1942d0ffc634fc54f3ffbdd7288cb6
Jason Lee <huacnlee@gmail.com>
2013-11-26 10:01:58 +0800

    SessionStore 有效期去掉，Devise 的记录登录状态里面有自己实现，这里不需要;

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,8 +2,7 @@
 # Be sure to restart your server when you modify this file.
 
 RubyChina::Application.config.session_store :cookie_store,
-                            :key => '_homeland_session',
-                            :expire_after => 86400*90
+                            :key => '_homeland_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

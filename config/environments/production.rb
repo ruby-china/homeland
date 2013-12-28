a4554c62dc904f6a15968fcda657234b8a01166d
Jason Lee <huacnlee@gmail.com>
2013-12-28 12:01:10 +0800

    Fix #250 产品环境不要自动变异 assets 文件;

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ RubyChina::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   config.action_controller.asset_host = Setting.upload_url
@@ -55,7 +55,7 @@ RubyChina::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true

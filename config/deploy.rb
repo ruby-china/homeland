9845895f4655bca095681366cc6deb9aced7f38c
Jason Lee <huacnlee@gmail.com>
2013-12-28 12:06:39 +0800

    修正 deploy script, 将 assets 目录放到 shared;

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,6 +48,7 @@ task :init_shared_path, :roles => :web do
 end
 
 task :link_shared_files, :roles => :web do
+  run "ln -sf #{shared_path}/assets #{deploy_to}/current/public/assets"
   run "ln -sf #{deploy_to}/shared/config/*.yml #{deploy_to}/current/config/"
   run "ln -sf #{deploy_to}/shared/config/initializers/secret_token.rb #{deploy_to}/current/config/initializers"
   run "ln -sf #{deploy_to}/shared/config/faye_thin.yml #{deploy_to}/current/faye_server/thin.yml"

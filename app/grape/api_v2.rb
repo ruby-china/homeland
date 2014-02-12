49cae224b47ca34ae78420375aaa46427e5dff7a
Bachue Zhou <bachue.shu@gmail.com>
2014-02-12 11:50:12 +0800

    现在Grape API出错将返回简单的出错消息而不是庞大的Web错误界面, fixes #269

--- app/grape/api_v2.rb
+++ app/grape/api_v2.rb
@@ -7,6 +7,17 @@ module RubyChina
     version "v2"
     error_format :json
 
+    rescue_from :all do |e|
+      case e
+      when Mongoid::Errors::DocumentNotFound
+        Rack::Response.new(['not found'], 404, {}).finish
+      else
+        # ExceptionNotifier.notify_exception(e) # Uncommit it when ExceptionNotification is available
+        Rails.logger.error "APIv2 Error: #{e}\n#{e.backtrace.join("\n")}"
+        Rack::Response.new(['error'], 500, {}).finish
+      end
+    end
+
     helpers APIHelpers
 
     # Authentication:
@@ -67,7 +78,7 @@ module RubyChina
       # Example
       #   /api/topics/1.json
       get ":id" do
-        @topic = Topic.find_by_id(params[:id])
+        @topic = Topic.find(params[:id])
         @topic.hits.incr(1)
         present @topic, :with => APIEntities::DetailTopic, :include_deleted => params[:include_deleted]
       end

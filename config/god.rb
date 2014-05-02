1444024659f520348c59c35af2e8a774436d68a2
Jason Lee <huacnlee@gmail.com>
2014-05-02 10:57:15 +0800

    God 监视 Faye 进程;

--- config/god.rb
+++ config/god.rb
@@ -37,3 +37,41 @@ God.watch do |w|
     end
   end
 end
+
+God.watch do |w|
+  w.name = "faya"
+  w.interval = 30.seconds
+  w.dir = [RAILS_ROOT, "faye_server"].join("/")
+  w.pid_file = "#{RAILS_ROOT}/tmp/pids/thin.6000.pid"
+  w.start = "bundle exec thin start -C thin.yml"
+  w.stop = "kill -9 `cat #{RAILS_ROOT}/tmp/pids/thin.6000.pid`"
+  
+  # determine the state on startup
+  w.transition(:init, { true => :up, false => :start }) do |on|
+    on.condition(:process_running) do |c|
+      c.running = true
+    end
+  end
+
+  # determine when process has finished starting
+  w.transition([:start, :restart], :up) do |on|
+    on.condition(:process_running) do |c|
+      c.running = true
+      c.interval = 5.seconds
+    end
+
+    # failsafe
+    on.condition(:tries) do |c|
+      c.times = 5
+      c.transition = :start
+      c.interval = 5.seconds
+    end
+  end
+
+  # start if process is not running
+  w.transition(:up, :start) do |on|
+    on.condition(:process_running) do |c|
+      c.running = false
+    end
+  end
+end

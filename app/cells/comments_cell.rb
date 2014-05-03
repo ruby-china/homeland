776c6efd231e0ed0a3b89eb154717ba7ec7cb40e
Jason Lee <huacnlee@gmail.com>
2014-05-03 11:02:25 +0800

    评论页面样式调整;

--- app/cells/comments_cell.rb
+++ app/cells/comments_cell.rb
@@ -1,7 +1,6 @@
-# -*- encoding : utf-8 -*-
+# coding: utf-8
 class CommentsCell < BaseCell
   def index(opts)
-    @title = opts[:title] || "评论"
     @commentable = opts[:commentable]
     @current_user = opts[:current_user]
     @comments = Comment.where(:commentable_type => @commentable.class.name,

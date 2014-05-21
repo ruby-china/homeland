96d9a8d6c99a6ad1592a0c5249e15b2e79bdbdf5
Jason Lee <huacnlee@gmail.com>
2014-05-21 10:33:46 +0800

    首页 ETag 处理;

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 # coding: utf-8
 class HomeController < ApplicationController
   def index
-    @excellent_topics = Topic.excellent.recent.fields_for_list.includes(:user).limit(20)
+    @excellent_topics = Topic.excellent.recent.fields_for_list.includes(:user).limit(20).to_a
     drop_breadcrumb("首页", root_path)
+    
+    fresh_when(:etag => [@excellent_topics,SiteConfig.index_html])
   end
 
   def api

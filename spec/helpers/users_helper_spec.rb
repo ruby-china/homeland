bd4232060630ddec79ff266e719a88e55aad4bab
yafeilee <lyfi2003@gmail.com>
2014-05-24 15:14:51 +0800

    修正测试失败

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -4,9 +4,9 @@ require "spec_helper"
 describe UsersHelper do
   describe "user_avatar_width_for_size" do
     it "should calculate avatar width correctly" do
-      helper.user_avatar_width_for_size(:normal).should == 48
+      helper.user_avatar_width_for_size(:normal).should == 64
       helper.user_avatar_width_for_size(:small).should == 16
-      helper.user_avatar_width_for_size(:large).should == 64
+      helper.user_avatar_width_for_size(:large).should == 81
       helper.user_avatar_width_for_size(:big).should == 120
       helper.user_avatar_width_for_size(233).should == 233
     end

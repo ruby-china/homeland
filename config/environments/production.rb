44dfa0d5d4f141731b38a4dfbe26a3f0788be279
Rei <chloerei@gmail.com>
2014-05-24 13:05:10 +0800

    Fix send file header for nginx

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,10 +13,10 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # For nginx:
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # If you have no front-end server that supports something like X-Sendfile,
   # just comment this out and Rails will serve the files

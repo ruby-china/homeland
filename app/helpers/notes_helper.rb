8348797321e86dcb2f5d29c22c2589ca159d0fff
Jason Lee <huacnlee@gmail.com>
2014-09-22 21:04:37 +0800

    修复后台捕捉到 note 里面的 gsub 异常;

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -6,4 +6,11 @@ module NotesHelper
   def render_node_name(name, id)
     link_to(name, node_topics_path(id), class: 'node')
   end
+  
+  def note_title_tag(note, opts = {})
+    opts[:limit] ||= 50
+    return "" if note.blank?
+    return "" if note.title.blank?
+    truncate(note.title.gsub('#',''), length: limit)
+  end
 end

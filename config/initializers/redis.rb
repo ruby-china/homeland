a42d2761c7e6a7c9c7092748922420086ab5b245
Jason Lee <huacnlee@gmail.com>
2014-09-25 10:21:38 +0800

    Puma 进程改成 4 个，Redis quit! on puma restart.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -5,7 +5,8 @@ require "redis/objects"
 
 redis_config = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
 
-Redis::Objects.redis = Redis.new(host: redis_config['host'], port: redis_config['port'])
+$redis = Redis.new(host: redis_config['host'], port: redis_config['port'])
+Redis::Objects.redis = $redis
 
 sidekiq_url = "redis://#{redis_config['host']}:#{redis_config['port']}/0"
 Sidekiq.configure_server do |config|
--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,10 @@ APP_ROOT = '/home/ruby/www/ruby-china/current'
 pidfile "#{APP_ROOT}/tmp/pids/puma.pid"
 state_path "#{APP_ROOT}/tmp/pids/puma.state"
 daemonize true
-workers 6
-threads 8,32
+workers 4
+threads 8,64
 preload_app!
 
+on_worker_boot do
+  defined?($redis) and $redis.client.quit!
+end

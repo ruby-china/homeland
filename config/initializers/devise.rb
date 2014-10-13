54f1b0af9de64245bd5b5451df50f56c2d25981b
Jason Lee <huacnlee@gmail.com>
2014-10-13 14:42:34 +0800

    Remove SSL verify skip code.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,13 +1,5 @@
-require 'openssl'
-module OpenSSL
-  module SSL
-    remove_const :VERIFY_PEER
-  end
-end
-
 # Devise::Async.backend = :sidekiq
 
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 # Use this hook to configure devise mailer, warden hooks and so forth. The first
 # four configuration values can also be set straight in your models.
 Devise.setup do |config|

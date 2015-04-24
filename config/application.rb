677b9fb3c48057a43d26c3e6a2f7884024d43785
Jason Lee <huacnlee@gmail.com>
2015-04-24 17:59:51 +0800

    eager_load lib/exception_notifier

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module RubyChina
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = "zh-CN"
+    
+    config.eager_load_paths += %W( #{config.root}/lib/exception_notifier )
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"

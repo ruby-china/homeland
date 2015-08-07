e46af2f9951557008c81f3628ce47835354307b7
diguage <leejun119@gmail.com>
2015-08-07 18:06:09 +0800

    修改源码地址

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ SiteConfig.save_default("index_html",<<-eos
   <p><img alt="Big_logo" src="/assets/big_logo.png"></p>
   <p></p>
   <p>Ruby China Group， 致力于构建完善的 Ruby 中文社区。</p>
-  <p>功能正在完善中，欢迎 <a href="http://github.com/huacnlee/ruby-china">贡献代码</a> 。</p>
+  <p>功能正在完善中，欢迎 <a href="https://github.com/ruby-china/ruby-china">贡献代码</a> 。</p>
   <p>诚邀有激情的活跃 Ruby 爱好者参与维护社区，联系 <b style="color:#c00;">lgn21st@gmail.com</b></p>
 </div>
 eos

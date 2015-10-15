16b908161e48fe2babc507319edac93f20637670
Rei <chloerei@gmail.com>
2015-10-15 18:26:00 +0800

    Replace gem source in vagrant setup

--- Vagrantfile
+++ Vagrantfile
@@ -5,5 +5,5 @@ Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/precise64"
   config.vm.hostname = "ruby-china-dev"
   config.vm.network "forwarded_port", guest: 3000, host: 3000
-  config.vm.provision "shell", path: "bin/provision.sh"
+  config.vm.provision "shell", path: "bin/provision.sh", privileged: false
 end
--- bin/provision.sh
+++ bin/provision.sh
@@ -1,13 +1,17 @@
-update-locale LC_ALL="en_US.utf8"
+sudo update-locale LC_ALL="en_US.utf8"
 
 # Add mongodb sources
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 
 # Add Ruby sources
-sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-add-repository -y ppa:brightbox/ruby-ng
 
-apt-get update
-apt-get install -y git redis-server memcached imagemagick mongodb-10gen ruby2.2 ruby2.2-dev nodejs
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y git redis-server memcached imagemagick mongodb-10gen ruby2.2 ruby2.2-dev nodejs
 
-gem install bundler
+gem sources --add https://ruby.taobao.org/ --remove https://rubygems.org/
+
+sudo gem install bundler
+bundle config mirror.https://rubygems.org https://ruby.taobao.org

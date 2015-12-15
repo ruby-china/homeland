f6b300e34319ee37378a5be904183a139ad3ed61
Seaborn Lee <seabornlee.cn@gmail.com>
2015-12-15 21:54:49 +0800

    Replace hard coded domain name with the configured value in config.yml

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,6 @@ class SearchController < ApplicationController
   def index
     keywords = params[:q] || ''
     keywords.gsub!('#', '%23')
-    redirect_to "https://www.google.com.hk/#hl=zh-CN&q=site:ruby-china.org+#{CGI.escape(keywords)}"
+    redirect_to "https://www.google.com.hk/#hl=zh-CN&q=site:#{Setting.domain}+#{CGI.escape(keywords)}"
   end
 end

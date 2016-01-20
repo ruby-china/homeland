7500249b7c9bf5ba03f6b023b35f43f1e3eef1fb
Jason Lee <huacnlee@gmail.com>
2016-01-20 16:50:16 +0800

    修正 /jobs 可能列表由于 SQL 错误为空的问题;

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -9,7 +9,9 @@ class JobsController < ApplicationController
     @node = Node.find(Node.jobs_id)
     @suggest_topics = Topic.where(node_id: @node.id).suggest.limit(3)
     suggest_topic_ids = @suggest_topics.map(&:id)
-    @topics = @node.topics.last_actived.fields_for_list.where("id NOT IN (?)", suggest_topic_ids).includes(:user).paginate(page: params[:page], per_page: 15)
+    @topics = @node.topics.last_actived.fields_for_list
+    @topics = @topics.where("id NOT IN (?)", suggest_topic_ids) if suggest_topic_ids.count > 0
+    @topics = @topics.includes(:user).paginate(page: params[:page], per_page: 15)
     set_seo_meta("#{@node.name} &raquo; #{t('menu.topics')}", "#{Setting.app_name}#{t('menu.topics')}#{@node.name}", @node.summary)
     render '/topics/index'
   end

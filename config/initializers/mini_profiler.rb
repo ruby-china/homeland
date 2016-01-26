e7d2d8423e26c4ad4c3cc393aab6768007ed421c
Jason Lee <huacnlee@gmail.com>
2016-01-26 15:06:51 +0800

    MiniProfiler 禁用 pp=env

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -6,4 +6,6 @@ if !Rails.env.test?
   Rack::MiniProfilerRails.initialize!(Rails.application)
   Rack::MiniProfiler.config.storage_options = { host: redis_config['host'], port: redis_config['port'] }
   Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+  Rack::MiniProfiler.config.disable_env_dump = true
+  Rack::MiniProfiler.config.skip_paths = ['/message-bus']
 end

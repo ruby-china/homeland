df70fe1ae6e45405612751d0779b3b1907d1b207
Jason Lee <huacnlee@gmail.com>
2016-01-26 17:58:57 +0800

    Fix typo in SearchIndexer

--- app/jobs/search_indexer.rb
+++ app/jobs/search_indexer.rb
@@ -9,7 +9,7 @@ class SearchIndexer < ActiveJob::Base
     when 'page'
       obj = Page.find(id)
     when 'user'
-      obj = Page.find(id)
+      obj = User.find(id)
     end
 
     if operation == 'update'

22cf8e53ad1a2ec672789dfe2bcd8ad96df37829
Jason Lee <huacnlee@gmail.com>
2016-01-28 23:02:58 +0800

    Fix users route

--- config/routes.rb
+++ config/routes.rb
@@ -135,7 +135,7 @@ Rails.application.routes.draw do
   get 'users/city/:id' => 'users#city', as: 'location_users'
   get 'users' => 'users#index', as: 'users'
 
-  resources :users, path: '' do
+  resources :users, path: '', as: 'users' do
     member do
       get :topics
       get :replies

fce348ff9c895b066a98c8c7a62ac8c0935c0a36
Jason Lee <huacnlee@gmail.com>
2016-02-15 18:06:21 +0800

    增加独立部署 ActionCable 服务器的配置;

---
+++ cable/config.ru
@@ -0,0 +1,4 @@
+require ::File.expand_path('../../config/environment',  __FILE__)
+Rails.application.eager_load!
+
+run ActionCable.server
---
+++ config/puma-cable.rb
@@ -0,0 +1,12 @@
+pidfile "#{app_root}/tmp/pids/puma-cable.pid"
+state_path "#{app_root}/tmp/pids/puma-cable.state"
+workers 1
+threads 16,64
+port 7001
+preload_app!
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end

a5c29868ccfa7ee964d03f6f642ac4ac0a7f03d0
Jason Lee <huacnlee@gmail.com>
2016-02-23 16:42:34 +0800

    ApplicationHelper sanitize_markdown 允许 width, height

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ require 'redcarpet'
 module ApplicationHelper
   ALLOW_TAGS = %w(p br img h1 h2 h3 h4 h5 h6 blockquote pre code b i
                   strong em table tr td tbody th strike del u a ul ol li span hr)
-  ALLOW_ATTRIBUTES = %w(href src class id title alt target rel data-floor)
+  ALLOW_ATTRIBUTES = %w(href src class width height id title alt target rel data-floor)
   EMPTY_STRING = ''.freeze
 
   def sanitize_markdown(body)

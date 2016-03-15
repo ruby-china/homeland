c94e5ee99b99935a546a4415f2d6709a949eb39d
Jason Lee <huacnlee@gmail.com>
2016-03-15 10:19:02 +0800

    config.action_cable.allowed_request_origins move to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,7 @@ Rails.application.configure do
   # Action Cable endpoint configuration
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = %w(#{Setting.protocol}://#{Setting.domain})
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
--- config/initializers/action_cable.rb
+++
@@ -1,1 +0,0 @@
-ActionCable.server.config.allowed_request_origins = ["#{Setting.protocol}://#{Setting.domain}"]

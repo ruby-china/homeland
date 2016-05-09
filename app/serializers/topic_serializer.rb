9dd25f3b9261cc625716f95e2405fffeac47a953
vincent178 <vincent.007.cn@gmail.com>
2016-05-09 10:05:21 +0800

    add topic likes_count (#632)

--- app/serializers/topic_serializer.rb
+++ app/serializers/topic_serializer.rb
@@ -1,7 +1,7 @@
 class TopicSerializer < BaseSerializer
   attributes :id, :title, :created_at, :updated_at, :replied_at, :replies_count,
              :node_name, :node_id, :last_reply_user_id, :last_reply_user_login,
-             :user, :deleted, :excellent, :abilities
+             :user, :deleted, :excellent, :abilities, :likes_count
 
   def user
     UserSerializer.new(object.user, root: false)

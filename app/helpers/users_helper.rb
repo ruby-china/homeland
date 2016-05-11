c7b120b2d6fd4d40bd54281267ccc02ab0abc638
songjiayang <songjiayang1@gmail.com>
2016-05-11 18:32:13 +0800

    Add user_avatar_tag method's unit test

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -41,8 +41,6 @@ module UsersHelper
   end
 
   def user_avatar_tag(user, size = :normal, opts = {})
-    link = opts[:link].nil? ? true : opts[:link]
-
     width = user_avatar_width_for_size(size)
     img_class = "media-object avatar-#{width}"
 
@@ -60,7 +58,7 @@ module UsersHelper
         image_tag(user.letter_avatar_url(width * 2), class: img_class)
       end
 
-    if link
+    if opts[:link] != false
       link_to(raw(img), user_path(user))
     else
       raw img

3d330e16bb3fabf6c81122a55a4071417dcb54a5
Jason Lee <huacnlee@gmail.com>
2016-05-25 11:44:16 +0800

    Fix captcha validation on password reset page.

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,9 +1,13 @@
 class PasswordsController < Devise::PasswordsController
   def create
-    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    self.resource = resource_class.find_or_initialize_with_errors(Devise.reset_password_keys, resource_params, :not_found)
+    if self.resource.persisted? && verify_rucaptcha?(resource)
+      self.resource.send_reset_password_instructions
+    end
+
     yield resource if block_given?
 
-    if verify_rucaptcha?(resource) && successfully_sent?(resource)
+    if successfully_sent?(resource)
       respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
     else
       respond_with(resource)

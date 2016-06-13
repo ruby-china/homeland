462e3e254dd7e56a2aeef3b5c3bcec92beef08a9
Jason Lee <huacnlee@gmail.com>
2016-06-13 10:52:08 +0800

    Cleanup assets precompile list. remove unused controller_stylesheets_link_tag, controller_javascript_include_tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,30 +24,6 @@ module ApplicationHelper
     flash_messages.join("\n").html_safe
   end
 
-  def controller_stylesheet_link_tag
-    fname = ''
-    case controller_name
-    when 'users', 'home', 'topics', 'pages', 'notes'
-      fname = "#{controller_name}.css"
-    when 'replies'
-      fname = 'topics.css'
-    end
-    return '' if fname.blank?
-    raw %(<link href="#{asset_path(fname)}" rel="stylesheet" data-turbolinks-track />)
-  end
-
-  def controller_javascript_include_tag
-    fname = ''
-    case controller_name
-    when 'pages', 'topics', 'notes'
-      fname = "#{controller_name}.js"
-    when 'replies'
-      fname = 'topics.js'
-    end
-    return '' if fname.blank?
-    raw %(<script src="#{asset_path(fname)}" data-turbolinks-track></script>)
-  end
-
   def admin?(user = nil)
     user ||= current_user
     user.try(:admin?)

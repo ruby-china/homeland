1fbd2c737c0d27c9646b237b55f68d9e9c442466
Jason Lee <huacnlee@gmail.com>
2016-06-28 11:50:48 +0800

    Run ActionCable in App

--- cable/config.ru
+++
@@ -1,4 +0,0 @@
-require ::File.expand_path('../../config/environment', __FILE__)
-Rails.application.eager_load!
-
-run ActionCable.server
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # Serve websocket cable requests in-process
-  mount ActionCable.server => '/cable'
-
   use_doorkeeper do
     controllers applications: 'oauth/applications', authorized_applications: 'oauth/authorized_applications'
   end

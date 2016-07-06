473af23b691cf27b25ab7db1e40b9547da63e519
Jason Lee <huacnlee@gmail.com>
2016-07-06 09:47:00 +0800

    允许上传 SVG

--- app/uploaders/base_uploader.rb
+++ app/uploaders/base_uploader.rb
@@ -16,6 +16,6 @@ class BaseUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(jpg jpeg gif png)
+    %w(jpg jpeg gif png svg)
   end
 end

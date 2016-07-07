de252aec037aa8885c84afe792a06151d5e0c4b8
Jason Lee <huacnlee@gmail.com>
2016-07-07 22:19:20 +0800

    Team has_many 通过 members 关联;

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,9 +2,17 @@ class Team < User
   has_many :team_users
   has_many :users, through: :team_users
 
+  %i(topics replies notes).each do |key|
+    has_many key, through: :users, source: key
+  end
+
   attr_accessor :owner_id
   after_create do
     self.team_users.create(user_id: owner_id, role: :owner) if self.owner_id.present?
+  end
+
+  def user_ids
+    @user_ids ||= self.users.pluck(:id)
   end
 
   def password_required?
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -21,4 +21,35 @@ describe Team, type: :model do
     it { expect(team.member?(team_owner.user)).to eq true }
     it { expect(team.member?(team_member.user)).to eq true }
   end
+
+  describe 'has_many' do
+    let!(:user) { create(:user) }
+    let!(:user1) { create(:user) }
+    let!(:team) { create(:team, owner_id: user.id) }
+    let!(:team_user) { create(:team_user, team: team, user: user1, role: :member) }
+
+    it 'topics should work' do
+      create_list(:topic, 2, user: user)
+      create_list(:topic, 1, user: user1)
+      expect(team.topics.count).to eq 3
+      expect(team.topics.pluck(:id)).to include(*user.topics.pluck(:id))
+      expect(team.topics.pluck(:id)).to include(*user1.topics.pluck(:id))
+    end
+
+    it 'replies should work' do
+      create_list(:reply, 2, user: user)
+      create_list(:reply, 1, user: user1)
+      expect(team.replies.count).to eq 3
+      expect(team.replies.pluck(:id)).to include(*user.replies.pluck(:id))
+      expect(team.replies.pluck(:id)).to include(*user1.replies.pluck(:id))
+    end
+
+    it 'notes should work' do
+      create_list(:note, 2, user: user)
+      create_list(:note, 1, user: user1)
+      expect(team.notes.count).to eq 3
+      expect(team.notes.pluck(:id)).to include(*user.notes.pluck(:id))
+      expect(team.notes.pluck(:id)).to include(*user1.notes.pluck(:id))
+    end
+  end
 end

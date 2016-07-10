b97457d177b0db27bbd9fbea06f84be996ca3abb
Jason Lee <huacnlee@gmail.com>
2016-07-10 19:16:27 +0800

    Fix Jobs controller test

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe JobsController, type: :controller do
   describe 'index' do
+    let(:node) { create(:node) }
     it 'should work' do
+      expect(Node).to receive(:jobs_id).and_return(node.id)
       get :index
       expect(response).to be_success
       expect(response.body).to match(/招聘/)

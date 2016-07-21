2c5ee77afabee243eb64f89e94cb445df826ec0c
Jason Lee <huacnlee@gmail.com>
2016-07-21 09:59:25 +0800

    登录有效期 60 天

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -98,7 +98,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
-  config.remember_for = 2.weeks
+  config.remember_for = 2.months
 
   # If true, a valid remember token can be re-used between multiple browsers.
   # config.remember_across_browsers = true

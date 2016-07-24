6803e011dc587f3f797ae299535c0e75de248b5a
Jason Lee <huacnlee@gmail.com>
2016-07-24 12:19:07 +0800

    去掉 User 无用的 temp_access_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -87,12 +87,6 @@ class User < ApplicationRecord
     self[:email] = val
   end
 
-  def temp_access_token
-    Rails.cache.fetch("user-#{id}-temp_access_token-#{Time.now.strftime('%Y%m%d')}") do
-      SecureRandom.hex
-    end
-  end
-
   def password_required?
     (authorizations.empty? || !password.blank?) && super
   end

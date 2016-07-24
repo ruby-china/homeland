8499cc85594bf868f4717295cd5d7bad87c2dadb
Jason Lee <huacnlee@gmail.com>
2016-07-24 09:59:02 +0800

    根据 UserAgent 单独给 Turbolinks App 设置样式。 这样一来，可以同时支持 Mobile Browser.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(app.js front.css admin.css)
+Rails.application.config.assets.precompile += %w(app.js front.css turbolinks-app.css admin.css)
 
 Rails.application.config.assets.quiet = true

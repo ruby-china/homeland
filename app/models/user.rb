f49acd3f674a27ecbe5e574de37e561cdbfbd156
Jason Lee <huacnlee@gmail.com>
2016-08-17 09:07:42 +0800

    修正用户名唯一验证的漏洞;

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,7 +52,7 @@ class User < ApplicationRecord
   validates :login, format: { with: ALLOW_LOGIN_CHARS_REGEXP, message: '只允许数字、大小写字母和下划线' },
                     length: { in: 3..20 },
                     presence: true,
-                    uniqueness: { case_sensitive: true }
+                    uniqueness: { case_sensitive: false }
 
   validates :name, length: { maximum: 20 }
 

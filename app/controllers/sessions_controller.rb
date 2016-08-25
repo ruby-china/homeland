92b2294a2708b70af98624c3cfeb2d3483215c4c
Jason Lee <huacnlee@gmail.com>
2016-08-25 13:10:58 +0800

    去掉登录验证码 fix #703

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < Devise::SessionsController
-  prepend_before_action :valify_captcha!, only: [:create]
   skip_before_action :set_locale, only: [:create]
 
   def new
@@ -15,15 +14,6 @@ class SessionsController < Devise::SessionsController
       format.html { redirect_to after_sign_in_path_for(resource) }
       format.json { render status: '201', json: resource.as_json(only: [:login, :email]) }
     end
-  end
-
-  def valify_captcha!
-    set_locale
-    unless verify_rucaptcha?
-      redirect_to new_user_session_path, alert: t('rucaptcha.invalid')
-      return
-    end
-    true
   end
 
   private

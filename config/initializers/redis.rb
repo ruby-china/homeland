22c7ee179d7d084e65262713e05c425817a6fbbe
Jason Lee <huacnlee@gmail.com>
2016-08-26 13:47:13 +0800

    修正 redis-objects 错误的 db 的问题;

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -6,6 +6,7 @@ require 'ruby-pinyin'
 redis_config = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
 
 $redis = Redis.new(host: redis_config['host'], port: redis_config['port'])
+$redis.select(0)
 Redis::Objects.redis = $redis
 
 sidekiq_url = "redis://#{redis_config['host']}:#{redis_config['port']}/0"
@@ -18,7 +19,7 @@ end
 
 # Redis Search
 PinYin.backend = PinYin::Backend::Simple.new
-redis_for_search = Redis::Namespace.new('rc-rs', redis: $redis)
+redis_for_search = Redis::Namespace.new('rc-rs', redis: Redis.new(host: redis_config['host'], port: redis_config['port']))
 redis_for_search.select(2)
 Redis::Search.configure do |config|
   config.redis = redis_for_search

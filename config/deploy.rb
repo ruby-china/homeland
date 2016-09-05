3b9cb794d821b816428019dd29edd05f02979e18
Jason Lee <huacnlee@gmail.com>
2016-09-05 11:05:58 +0800

    Fix Cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,42 +1,20 @@
-# config valid only for current version of Capistrano
 lock '~> 3.6.1'
 
 set :application, 'ruby-china'
 set :repo_url, 'git://github.com/ruby-china/ruby-china.git'
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/data/www/#{ fetch(:application) }"
 
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# puma
 set :puma_role, :app
-set :puma_config_file, 'config/puma-web.rb'
+set :puma_conf, "#{release_path}/config/puma-web.rb"
 
-# Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/redis.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
+append :linked_files, *%w(config/secrets.yml config/database.yml config/config.yml
+                          config/elasticsearch.yml config/oneapm.yml config/redis.yml)
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/assets'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 10
 
 after 'deploy:publishing', 'deploy:restart'

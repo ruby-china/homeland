e700538bba9bc09eb643f32b8de8a6e643ceba97
Martin Hong <hongzeqin@gmail.com>
2016-09-10 23:09:34 +0800

    add new scope :without_team to limit only personal accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,6 +55,7 @@ class User < ApplicationRecord
   validates :name, length: { maximum: 20 }
 
   scope :hot, -> { order(replies_count: :desc).order(topics_count: :desc) }
+  scope :without_team, -> { where(type: nil) }
   scope :fields_for_list, lambda {
     select(:type, :id, :name, :login, :email, :email_md5, :email_public, :avatar, :verified, :state,
            :tagline, :github, :website, :location, :location_id, :twitter, :co)

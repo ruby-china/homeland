ca7a4f4608be4f920c19275bf74cedc929ca1197
Jason Lee <huacnlee@gmail.com>
2016-09-14 14:30:13 +0800

    Remove unused code

--- app/controllers/admin/topics_controller.rb
+++ app/controllers/admin/topics_controller.rb
@@ -48,13 +48,11 @@ module Admin
 
     def suggest
       @topic.update_attribute(:suggested_at, Time.now)
-      CacheVersion.topic_last_suggested_at = Time.now
       redirect_to(@topic, notice: "Topic:#{params[:id]} suggested.")
     end
 
     def unsuggest
       @topic.update_attribute(:suggested_at, nil)
-      CacheVersion.topic_last_suggested_at = Time.now
       redirect_to(@topic, notice: "Topic:#{params[:id]} unsuggested.")
     end
 

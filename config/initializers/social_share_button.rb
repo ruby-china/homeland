d324a3ce1d31e44fffc94b2836ad824635df270a
songjiayang <songjiayang1@gmail.com>
2016-09-21 18:33:37 +0800

    add weichat share button

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_plus weibo douban)
+  config.allow_sites = %w(twitter facebook google_plus weibo douban weichat)
 end

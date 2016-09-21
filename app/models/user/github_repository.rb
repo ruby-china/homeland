e0493fc815bb3b7665d02496f17183f82675d30d
Jason Lee <huacnlee@gmail.com>
2016-09-21 23:10:45 +0800

    Fix User.fetch_github_repositories, fix admin delete user button #742

--- app/models/user/github_repository.rb
+++ app/models/user/github_repository.rb
@@ -2,6 +2,9 @@ class User
   module GithubRepository
     extend ActiveSupport::Concern
 
+    included do
+    end
+
     # GitHub 项目
     def github_repositories
       cache_key = github_repositories_cache_key
@@ -17,38 +20,40 @@ class User
       "github-repos:#{github}:1"
     end
 
-    def self.fetch_github_repositories(user_id)
-      user = User.find_by(id: user_id)
-      return unless user
-
-      url = user.github_repo_api_url
-      begin
-        json = Timeout.timeout(10) { open(url).read }
-      rescue => e
-        Rails.logger.error("GitHub Repositiory fetch Error: #{e}")
-        $file_store.write(user.github_repositories_cache_key, [], expires_in: 1.minutes)
-        return
-      end
-
-      items = JSON.parse(json)
-      items = items.collect do |a1|
-        {
-          name: a1['name'],
-          url: a1['html_url'],
-          watchers: a1['watchers'],
-          language: a1['language'],
-          description: a1['description']
-        }
-      end
-      items.sort! { |a, b| b[:watchers] <=> a[:watchers] }.take(10)
-      $file_store.write(user.github_repositories_cache_key, items, expires_in: 15.days)
-      items
-    end
-
     def github_repo_api_url
       github_login = self.github || self.login
       resource_name = organization? ? 'orgs' : 'users'
       "https://api.github.com/#{resource_name}/#{github_login}/repos?type=owner&sort=pushed&client_id=#{Setting.github_token}&client_secret=#{Setting.github_secret}"
+    end
+
+    module ClassMethods
+      def fetch_github_repositories(user_id)
+        user = User.find_by(id: user_id)
+        return unless user
+
+        url = user.github_repo_api_url
+        begin
+          json = Timeout.timeout(10) { open(url).read }
+        rescue => e
+          Rails.logger.error("GitHub Repositiory fetch Error: #{e}")
+          $file_store.write(user.github_repositories_cache_key, [], expires_in: 1.minutes)
+          return
+        end
+
+        items = JSON.parse(json)
+        items = items.collect do |a1|
+          {
+            name: a1['name'],
+            url: a1['html_url'],
+            watchers: a1['watchers'],
+            language: a1['language'],
+            description: a1['description']
+          }
+        end
+        items.sort! { |a, b| b[:watchers] <=> a[:watchers] }.take(10)
+        $file_store.write(user.github_repositories_cache_key, items, expires_in: 15.days)
+        items
+      end
     end
   end
 end

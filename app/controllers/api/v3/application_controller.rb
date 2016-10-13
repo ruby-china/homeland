a6f0b096b773b7476276ee58e2a1238c632af0fb
Jason Lee <huacnlee@gmail.com>
2016-10-13 12:56:48 +0800

    完善 serializers 的 API 文档;

--- app/controllers/api/v3/application_controller.rb
+++ app/controllers/api/v3/application_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V3
-    # @private
+    # @abstract
     class ApplicationController < ActionController::API
       class ParameterValueNotAllowed < ActionController::ParameterMissing
         attr_reader :values
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,9 @@
+# 用户 API 返回数据结构
+# == attributes
+# - *id* [Integer] 用户编号
+# - *login* [String] 用户名
+# - *name* [String] 用户姓名
+# - *avatar_url* [String] 头像 URL
 class UserSerializer < BaseSerializer
   attributes :id, :login, :name, :avatar_url
 

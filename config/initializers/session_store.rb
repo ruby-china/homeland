1bf1ee2f73cabf488a33413f367405a71f1e3a31
Jason Lee <huacnlee@gmail.com>
2016-10-16 11:10:14 +0800

    改用 redis_session_store 减少安全问题; (#774)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_homeland_session'
+# Rails.application.config.session_store :cookie_store, key: '_homeland_session'
+redis_config = Rails.application.config_for(:redis)
+Rails.application.config.session_store :redis_session_store, {
+  key: '_homeland_session',
+  redis: {
+    expire_after: 60.days,
+    key_prefix: 'rc:session',
+    url: "redis://#{redis_config['host']}:#{redis_config['port']}/0",
+  }
+}

6533e0f1dc36b21702231127f46a84233ba7d5ec
Jason Lee <huacnlee@gmail.com>
2016-10-19 17:54:27 +0800

    Fix elasticsearch read config/elasticsearch.yml

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,6 @@
+require 'elasticsearch/rails/instrumentation'
+
+config = Rails.application.config_for(:elasticsearch)
+
+Elasticsearch::Model.client = Elasticsearch::Client.new host: config['host']
 Elasticsearch::Model.settings[:inheritance_enabled] = false

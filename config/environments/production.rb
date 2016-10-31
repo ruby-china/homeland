ecf8e6439fe86b783e2e3c695b788f7ae8f8f880
Jason Lee <huacnlee@gmail.com>
2016-10-31 16:10:48 +0800

    [DEL] email_server, email_username, email_password 几个配置项; 让 ActionMailer 配置支持通过 config.yml 任意配置

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,13 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: Setting.domain, protocol: Setting.protocol }
-  config.action_mailer.delivery_method   = :postmark
-  config.action_mailer.postmark_settings = { api_key: Setting.email_password }
+
+  config.action_mailer.delivery_method   = Setting.mailer_provider
+  if Setting.mailer_provider == 'postmark'
+    config.action_mailer.postmark_settings = Setting.mailer_options
+  else
+    config.action_mailer.smtp_settings = Setting.mailer_options
+  end
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).

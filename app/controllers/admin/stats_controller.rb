f4305115294b0de746ca0b88388a6ff5db035705
Jason Lee <huacnlee@gmail.com>
2016-11-03 19:49:18 +0800

    Fix Topic, Reply charts bad data

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -17,7 +17,7 @@ module Admin
                   when 'week' then "date_trunc('week', created_at)"
                   else "date_trunc('month', created_at)"
                   end
-      results = klass.unscoped.where('created_at >= ?', date_from)
+      results = klass.unscoped.where('created_at >= ? and created_at <= ?', date_from, Date.current)
                               .group("date")
                               .select("#{group_cmd} AS date, count(id) AS count").all
       render json: results.as_json

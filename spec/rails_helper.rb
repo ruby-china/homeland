b3bb35ca6110dd299c8154a372520646e0750b54
Jason Lee <huacnlee@gmail.com>
2016-11-08 17:22:55 +0800

    Don't allow use upyun as upload_provider in test environment!

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
+abort('Do not use "upyun" as upload_provider!') if Setting.upload_provider == 'upyun'
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rspec'

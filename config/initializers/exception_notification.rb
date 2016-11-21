17ed1f5a6ae73cf101a43b40df655d30464b8525
Jason Lee <huacnlee@gmail.com>
2016-11-21 10:37:38 +0800

    protect_from_forgery with: :exception and ExceptionNotification ignore ActionController::InvalidAuthenticityToken

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -4,10 +4,10 @@ require 'exception_notifier/database_notifier'
 
 ExceptionNotification.configure do |config|
   config.ignored_exceptions += %w(ActionView::TemplateError
-                                  ActionController::BadRequest ActionView::MissingTemplate ActionController::UrlGenerationError
+                                  ActionController::InvalidAuthenticityToken
+                                  ActionController::BadRequest
+                                  ActionView::MissingTemplate
+                                  ActionController::UrlGenerationError
                                   ActionController::UnknownFormat)
-  # config.ignore_if do |exception, options|
-  #   not Rails.env.production?
-  # end
   config.add_notifier :database, {}
 end

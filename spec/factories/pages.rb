460d7f3f7a4351eb87e05a0c7299fa12274fe683
Jason Lee <huacnlee@gmail.com>
2016-12-15 11:25:46 +0800

    Remove body_html from Page factory

--- spec/factories/pages.rb
+++ spec/factories/pages.rb
@@ -3,6 +3,5 @@ FactoryGirl.define do
     sequence(:slug) { |n| "slug#{n}" }
     sequence(:title) { |n| "title#{n}" }
     sequence(:body) { |n| "body#{n}" }
-    sequence(:body_html) { |n| "body_html#{n}" }
   end
 end

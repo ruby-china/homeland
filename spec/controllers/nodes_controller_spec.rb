1fd21d2af69fc863ebd0a2697c32491422850522
debbbbie <debbbbie@163.com>
2016-12-21 21:05:21 +0800

    Add Coverage test to Nodes, Notes and Topics Controllers

--- spec/controllers/nodes_controller_spec.rb
+++ spec/controllers/nodes_controller_spec.rb
@@ -1,8 +1,23 @@
 require 'rails_helper'
 
 describe NodesController, type: :controller do
+  let(:node) { create(:node) }
+  let(:user) { create(:user) }
+
   it 'should have an index action' do
     get :index
+    expect(response).to be_success
+  end
+
+  it 'should have an block action' do
+    sign_in user
+    post :block, params: { id: node }
+    expect(response).to be_success
+  end
+
+  it 'should have an unblock action' do
+    sign_in user
+    post :unblock, params: { id: node }
     expect(response).to be_success
   end
 end

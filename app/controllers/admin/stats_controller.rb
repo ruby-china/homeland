6b754730ce020e4b7b7e73bd40e365747fec4043
Jason Lee <huacnlee@gmail.com>
2017-01-04 13:55:13 +0800

    调整后台统计数字的排版;

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -5,21 +5,11 @@ module Admin
     # model - Model 名称
     # by - day, week, month
     def index
-      date_from = case params[:by]
-                  when 'month' then 12.months.ago.beginning_of_month.to_date
-                  when 'week' then 6.months.ago.beginning_of_month.to_date
-                  else 30.days.ago.beginning_of_month.to_date
-                  end
-      res = {}
-      group_cmd = case params[:by]
-                  when 'day' then "date(created_at at time zone 'CST')"
-                  when 'week' then "date_trunc('week', created_at)"
-                  else "date_trunc('month', created_at)"
-                  end
-      results = klass.unscoped.where('created_at >= ? and created_at <= ?', date_from, Date.current)
-                              .group("date")
-                              .select("#{group_cmd} AS date, count(id) AS count").all
-      render json: results.as_json
+      result = { model: params[:model] }
+      result[:count] = klass.unscoped.count
+      result[:week_count] = klass.unscoped.where("created_at >= ?", Date.today.beginning_of_week).count
+      result[:month_count] = klass.unscoped.where("created_at >= ?", Date.today.beginning_of_month).count
+      render json: result.as_json
     end
 
     def klass

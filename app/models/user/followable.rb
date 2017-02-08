fe6450e1ed3d850e0f18a94f8b915b9424bd162d
Jason Lee <huacnlee@gmail.com>
2017-02-08 11:12:04 +0800

    Fix indent

--- app/models/user/followable.rb
+++ app/models/user/followable.rb
@@ -5,7 +5,7 @@ class User
     included do
       action_store :follow, :topic
       action_store :follow, :user, counter_cache: 'followers_count',
-                                 user_counter_cache: 'following_count'
+                                   user_counter_cache: 'following_count'
     end
 
     def follow_user(user)

90b07a41a3b0ca56527309e1e6b8a718b8852ee8
Jason Lee <huacnlee@gmail.com>
2017-02-14 11:17:08 +0800

    Save more Exception environments.

--- lib/exception_notifier/database_notifier.rb
+++ lib/exception_notifier/database_notifier.rb
@@ -10,14 +10,34 @@ module ExceptionNotifier
       @title = exception.message
       messages = []
       messages << exception.inspect
+      messages << "\n"
+      messages << "--------------------------------------------------"
+      messages << headers_for_env(_options[:env])
+      messages << "--------------------------------------------------"
       unless exception.backtrace.blank?
         messages << "\n"
         messages << exception.backtrace
       end
 
+      puts messages.join("\n")
+
       if Rails.env.production?
         ExceptionLog.create(title: @title, body: messages.join("\n"))
       end
+    end
+
+    def headers_for_env(env)
+      return '' if env.blank?
+
+      headers = []
+      headers << "Method:     #{env['REQUEST_METHOD']}"
+      headers << "URL:        #{env['rack.url_scheme']}://#{env['HTTP_HOST']}#{env['REQUEST_URI']}"
+      headers << "User-Agent: #{env['HTTP_USER_AGENT']}"
+      headers << "Language:   #{env['HTTP_ACCEPT_LANGUAGE']}"
+      headers << "Server:     #{Socket.gethostname}"
+      headers << "Process:    #{$$}"
+
+      headers.join("\n")
     end
   end
 end

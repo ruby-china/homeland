09d4482e1cb411561a5cf6e2584d972f12764343
Jason Lee <huacnlee@gmail.com>
2017-02-17 16:52:15 +0800

    修正搜索页面，缺少 q 参数的时候的异常;

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,8 @@ class SearchController < ApplicationController
   before_action :authenticate_user!, only: [:users]
 
   def index
+    params[:q] ||= ''
+
     search_modules = [Topic, User]
     search_modules << Page if Setting.has_module?(:wiki)
     search_params = {

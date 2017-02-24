e9e55f39422b12ff92b54a61ef736472c0c8957a
Jason Lee <huacnlee@gmail.com>
2017-02-24 15:36:25 +0800

    config.read_encrypted_secrets = false 保持原有的方式，因为 Docker 的流程 secrets 是保密的

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
-  config.read_encrypted_secrets = true
+  config.read_encrypted_secrets = false
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.

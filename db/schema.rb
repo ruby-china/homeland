7d10ed5ea79b52ad7d7bcffe8c16a2e46f531d7e
Jason Lee <huacnlee@gmail.com>
2017-02-28 00:19:41 +0800

    homeland-press use gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224074052) do
+ActiveRecord::Schema.define(version: 20170227080755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -191,6 +191,25 @@ ActiveRecord::Schema.define(version: 20170224074052) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["user_id"], name: "index_photos_on_user_id", using: :btree
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",                                   null: false
+    t.string   "slug",                                    null: false
+    t.text     "body",                                    null: false
+    t.string   "summary",        limit: 5000
+    t.string   "banner"
+    t.integer  "user_id"
+    t.integer  "likes_count",                 default: 0, null: false
+    t.integer  "comments_count",              default: 0, null: false
+    t.integer  "status",                      default: 0, null: false
+    t.datetime "published_at",                            null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+    t.index ["published_at"], name: "index_posts_on_published_at", using: :btree
+    t.index ["slug"], name: "index_posts_on_slug", using: :btree
+    t.index ["status"], name: "index_posts_on_status", using: :btree
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
   create_table "replies", force: :cascade do |t|

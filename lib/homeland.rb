400f2464aeae6a197d03732454452245b1649f61
Jason Lee <huacnlee@gmail.com>
2017-03-07 13:37:08 +0800

    version 3.0.0.beta

--- lib/homeland.rb
+++ lib/homeland.rb
@@ -1,9 +1,7 @@
+require 'homeland/version'
+
 module Homeland
   class << self
-    def version
-      '2.7.0'
-    end
-
     def file_store
       @file_store ||= ActiveSupport::Cache::FileStore.new(Rails.root.join('tmp/cache'))
     end

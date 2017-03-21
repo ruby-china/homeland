9638fe8ba0a295f7d612f22fcd3747f92c5671a1
Jason Lee <huacnlee@gmail.com>
2017-03-21 21:52:35 +0800

    Remove SiteNode from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,3 @@ Node.create(name: 'Apple', summary: '...', section_id: s2.id)
 Node.create(name: 'Google', summary: '...', section_id: s2.id)
 Node.create(name: 'Coding', summary: '...', section_id: s2.id)
 Node.create(name: 'PlayStation / XBox', summary: '...', section_id: s2.id)
-
-# ============ Init Site Node ================
-SiteNode.create(name: "News", sort: 100)
-SiteNode.create(name: "Blog", sort: 99)
-SiteNode.create(name: "Other", sort: 94)

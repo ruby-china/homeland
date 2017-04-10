f8c7ad9628595d97af68ec4005a7e2b354a86f42
Jason Lee <huacnlee@gmail.com>
2017-04-10 18:15:44 +0800

    调整头像上传逻辑，确保每次上传都是新的头像地址，已解决 #637 的问题。 Fragmement Cache 带上 user_avatar 的值，便于用户头像变更的时候能更新缓存。

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,8 @@
 class AvatarUploader < BaseUploader
   def filename
     if super.present?
-      "avatar/#{model.id}.jpg"
+      @name ||= Digest::MD5.hexdigest(current_path)
+      "avatar/#{@name}.#{file.extension.downcase}"
     end
   end
 end

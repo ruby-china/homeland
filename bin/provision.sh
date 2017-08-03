d4748c034b164a5339215966cd2dbc25905615b8
Lonre Wang <lonre@users.noreply.github.com>
2017-08-03 11:08:10 +0800

    更新 Vagrant 开发环境 (#945)
    * Fix nokogiri installation error
    Error: `zlib is missing; necessary for building libxml2`
    
    * Upgrade vagrant environment
    1. Ubuntu precise64 => trusty64
    2. Elasticsearch 2.x => 5.x
    3. Ruby 2.3 => 2.4
    4. JRE 7 => 8

--- bin/provision.sh
+++ bin/provision.sh
@@ -1,8 +1,8 @@
 sudo update-locale LC_ALL="en_US.utf8"
 
 # Add Elasticsearch sources
-wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
 
 # Add PG sources
 echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" | sudo tee -a /etc/apt/sources.list.d/pgdb.list
@@ -11,22 +11,25 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 # Add Ruby sources
 sudo add-apt-repository ppa:brightbox/ruby-ng
 sudo add-apt-repository ppa:chris-lea/redis-server
+sudo add-apt-repository ppa:openjdk-r/ppa
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y git \
+sudo apt-get install -y apt-transport-https \
+                        git \
                         redis-server \
                         memcached \
                         imagemagick \
                         nodejs \
                         libpq-dev \
+                        zlib1g-dev \
                         postgresql-9.4 \
                         elasticsearch \
-                        openjdk-7-jre-headless \
-                        ruby2.3 \
-                        ruby2.3-dev
+                        openjdk-8-jre-headless \
+                        ruby2.4 \
+                        ruby2.4-dev
 
-sudo update-rc.d elasticsearch defaults
+echo "ES_JAVA_OPTS='-Xms128m -Xmx1g'" | sudo tee -a /etc/default/elasticsearch
 sudo service elasticsearch start
 
 sudo su postgres -c "createuser -d -R -S $USER"

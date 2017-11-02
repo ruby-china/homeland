64a465728cb076d446c7a2a7bc0a97b035665ce8
Jason Lee <huacnlee@gmail.com>
2017-11-02 12:17:44 +0800

    Change Twemoji CDN to cdn.bootcss.com

--- config/initializers/twemoji.rb
+++ config/initializers/twemoji.rb
@@ -1,7 +1,7 @@
 require 'twemoji/svg'
 
 Twemoji.configure do |config|
-  config.asset_root = 'https://twemoji.b0.upaiyun.com/2'
+  config.asset_root = 'https://cdn.bootcss.com/twemoji/2.5.0/2'
   config.file_ext   = 'svg'
   config.class_name = 'twemoji'
 end

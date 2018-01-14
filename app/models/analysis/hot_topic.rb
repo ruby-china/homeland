b23597b2e6bbb34df25bdf28581810bb6d4aff46
Spirit <neverlandxy.naix@gmail.com>
2018-01-15 00:03:05 +0800

    finish week_hot_topic

--- app/models/analysis/hot_topic.rb
+++ app/models/analysis/hot_topic.rb
@@ -2,54 +2,79 @@
 
 module Analysis
   class HotTopic
+    include Singleton
 
-    def week_hot_topic(size = 100)
-      result = TopicPageView.search({
-        query: week_range,
+    HOT_TOPIC_SEARCH_SIZE = 700 # 聚合热门话题搜索数量
+    HOT_TOPIC_SIZE = 100        # 热门话题数量
+    WEEK_DAY_NUM = 7
+
+    def week_hot_topic
+      hot_topic = {}
+      week_search_result.each_with_index do |result, index|
+        result.each do |topic_id, pv|
+          hot_topic[topic_id] ||= 0
+          hot_topic[topic_id] += pv * (WEEK_DAY_NUM - index)
+        end
+      end
+      hot_topic.sort_by { |_, v| -v }[0, HOT_TOPIC_SIZE].map { |r| r[0] }
+    end
+
+    # 返回一个 size 为 7 的 Array
+    # Array 第一个元素为当天热门话题所对应的 Hash, Hash 结构为 { topic_id1 => pv, topic_id1 => pv, ...}
+    # Array 第二个元素为当天热门话题所对应的 Hash, 依次类推
+    #
+    # [
+    #   { topic_id1 => pv, topic_id1 => pv, ...},
+    #   ...
+    #   { topic_id1 => pv, topic_id1 => pv, ...}
+    # ]
+    def week_search_result
+      result = TopicPageView.search(week_search_query)
+      result = result.response.aggregations.deep_symbolize_keys
+      result[:by_day][:buckets].map do |bucket|
+        bucket[:topic_ids][:buckets].reduce({}) do |result, tb|
+          result[tb[:key]] = tb[:doc_count]
+          result
+        end
+      end
+    end
+
+    def week_search_query
+      {
+        size: 0,
+        query: {
+          range: {
+            created_at: {
+              gt: "now-7d/d",
+              lte: "now/d",
+              time_zone: "+08:00"
+            }
+          }
+        },
         aggs: {
           by_day: {
-            date_histogram: day_histogram,
+            date_histogram: {
+              field: "created_at",
+              interval: "day",
+              format: "year_month_day",
+              time_zone: "+08:00",
+              min_doc_count: 0,
+              order: {
+                _key: "desc"
+              }
+            },
             aggs: {
               topic_ids: {
-                terms: topic_id_terms(size)
+                terms: {
+                  field: "topic_id",
+                  size: HOT_TOPIC_SEARCH_SIZE,
+                  order: {
+                    _count: "desc"
+                  }
+                }
               }
             }
           }
-        }
-      })
-
-      result.response.aggregations.by_day.buckets.as_json
-    end
-
-    def week_range
-      {
-        range: {
-          created_at: {
-            gte: "now-7d/d",
-            lte: "now/d"
-          }
-        }
-      }
-    end
-
-    def day_histogram
-      {
-        field: "created_at",
-        interval: "day",
-        format: "yyyy-MM-dd",
-        min_doc_count: 0,
-        order: {
-          _key: 'desc'
-        }
-      }
-    end
-
-    def topic_id_terms(size = 100)
-      {
-        field: "topic_id",
-        size: size,
-        order: {
-          _count: 'desc'
         }
       }
     end
--- app/models/topic_page_view.rb
+++ app/models/topic_page_view.rb
@@ -5,9 +5,32 @@ require 'elasticsearch/persistence/model'
 class TopicPageView
   include Elasticsearch::Persistence::Model
 
-  attribute :topic_id, Integer
+  attribute :topic_id,   Integer
+  attribute :created_at, Time, default: ->(o, a) { Time.now }
+  attribute :updated_at, Time, default: ->(o, a) { Time.now }
 
   validates :topic_id, :created_at, presence: true
 
   create_index!
+
+  class << self
+    def fake_pv
+      data = self.new(
+        topic_id:   rand(3_000),
+        created_at: Time.at(Time.now.to_i - rand(1.month.to_i))
+      ).as_json
+
+      data.delete(:id)
+      data
+    end
+
+    def fake_data
+      Elasticsearch::Persistence.client.bulk(
+        index: 'topic_page_views',
+        type:  'topic_page_view',
+        body:  (1..10_000).map { { index: { data: fake_pv } } },
+        refresh: true
+      )
+    end
+  end
 end

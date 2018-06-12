07415541aeeb4e7f9fd7330f89b0bd1e32b37568
Jason Lee <huacnlee@gmail.com>
2018-06-12 15:45:40 +0800

    Update Twemoji CDN to UpYun

--- config/initializers/twemoji.rb
+++ config/initializers/twemoji.rb
@@ -3,7 +3,7 @@
 require "twemoji/svg"
 
 Twemoji.configure do |config|
-  config.asset_root = "https://cdn.bootcss.com/twemoji/2.5.0/2"
+  config.asset_root = "https://twemoji.b0.upaiyun.com/2"
   config.file_ext   = "svg"
   config.class_name = "twemoji"
 end

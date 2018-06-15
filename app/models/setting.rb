375f43f23d5790ba09e90ed9383dbe4cbb8518c4
Jason Lee <huacnlee@gmail.com>
2018-06-15 12:00:08 +0800

    改进 Setting 的默认 key 和 value

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -4,18 +4,42 @@
 class Setting < RailsSettings::Base
   source Rails.root.join("config/config.yml")
 
+  class << self
+    def field(key, default: nil, type: :string, separator: nil)
+      self.class.define_method(key) do
+        val = self[key]
+        default = default.call if default.is_a?(Proc)
+        return default if val.nil?
+        val
+      end
+
+      if type == :boolean
+        self.class.define_method("#{key}?") do
+          val = self.send(key.to_sym)
+          val == "true" || val == "1"
+        end
+      elsif type == :array
+        self.class.define_method("#{key.to_s.singularize}_list") do
+          val = self.send(key.to_sym) || ""
+          separator = SEPARATOR_REGEXP if separator.nil?
+          val.split(separator).reject { |str| str.empty? }
+        end
+      end
+    end
+  end
+
   # List setting value separator chars
   SEPARATOR_REGEXP = /[\s,]/
 
   # keys that allow update in admin
   KEYS_IN_ADMIN = %w[
     app_name
+    navbar_brand_html
     default_locale
     auto_locale
     admin_emails
     custom_head_html
     navbar_html
-    navbar_brand_html
     footer_html
     index_html
     wiki_index_html
@@ -37,6 +61,17 @@ class Setting < RailsSettings::Base
     topic_create_hour_limit_count
   ]
 
+  field :app_name, default: "Homeland"
+  field :navbar_brand_html, default: -> { %(<a href="/" class="navbar-brand"><b>#{self.app_name}</b></a>) }
+  field :default_locale, default: "zh-CN"
+  field :auto_locale, default: "false", type: :boolean
+  field :reject_newbie_reply_in_the_evening, default: "false", type: :boolean
+  field :topic_create_rate_limit, default: "false", type: :boolean
+  field :admin_emails, default: "admin@admin.com", type: :array
+  field :ban_reasons, default: "标题或正文描述不清楚", type: :array, separator: "\n"
+  field :modules, default: "all", type: :array
+  field :profile_fields, default: "all", type: :array
+
   class << self
     def protocol
       self.https == true ? "https" : "http"
@@ -47,8 +82,8 @@ class Setting < RailsSettings::Base
     end
 
     def has_admin?(email)
-      return false if self.admin_emails.blank?
-      self.admin_emails.split(SEPARATOR_REGEXP).include?(email)
+      return false if self.admin_email_list.blank?
+      self.admin_email_list.include?(email)
     end
 
     def has_module?(name)
@@ -56,17 +91,9 @@ class Setting < RailsSettings::Base
       self.module_list.include?(name.to_s)
     end
 
-    def module_list
-      self.modules.split(SEPARATOR_REGEXP)
-    end
-
-    def ban_reason_list
-      (self.ban_reasons || "").split("\n")
-    end
-
     def has_profile_field?(name)
       return true if self.profile_fields.blank? || self.profile_fields == "all"
-      self.profile_fields.split(SEPARATOR_REGEXP).include?(name.to_s)
+      self.profile_field_list.include?(name.to_s)
     end
 
     def sso_enabled?
@@ -76,22 +103,6 @@ class Setting < RailsSettings::Base
 
     def sso_provider_enabled?
       self.sso["enable_provider"] == true
-    end
-
-    def reject_newbie_reply_in_the_evening?
-      self[:reject_newbie_reply_in_the_evening] == "true"
-    end
-
-    def topic_create_rate_limit?
-      self[:topic_create_rate_limit] == "true"
-    end
-
-    def default_locale
-      self[:default_locale] || "zh-CN"
-    end
-
-    def auto_locale?
-      self[:auto_locale] == "true"
     end
   end
 end

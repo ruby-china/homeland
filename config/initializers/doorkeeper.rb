98e37d642701d73e26eeb6a146ac53833d1dbd8b
Jason Lee <huacnlee@gmail.com>
2018-09-12 16:54:56 +0800

    新增 OAuth 2 Application 的 confidential 选项，修正 OAuth2 用户管理界面可能的安全隐患

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -21,11 +21,9 @@ Doorkeeper.configure do
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
-  # admin_authenticator do
-  #   # Put your admin authentication logic here.
-  #   # Example implementation:
-  #   Admin.find_by_id(session[:admin_id]) || redirect_to(new_admin_session_url)
-  # end
+  admin_authenticator do
+    current_user
+  end
 
   # Authorization Code expiration time (default 10 minutes).
   # authorization_code_expires_in 10.minutes

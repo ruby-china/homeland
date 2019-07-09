f083c7220eea07bf310ae83b07ae1b39c5c70b1d
Jason Lee <huacnlee@gmail.com>
2019-07-09 09:33:08 +0800

    Fix #1119 with correct Uploader extentions limit

---
+++ spec/uploaders/avatar_uploader_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "AvatarUploader" do
+  let(:not_an_image) { fixture_file_upload('test.html', 'text/html') }
+  let(:image) { fixture_file_upload('test.png', 'image/png') }
+
+  it "extension limit" do
+    user = build(:user, avatar: not_an_image)
+    expect(user.valid?).to eq(false)
+    expect(user.errors.full_messages_for(:avatar)).to eq(["头像仅允许图片文件上传 [jpg, jpeg, gif, png, svg]"])
+
+    user = build(:user, avatar: image)
+    expect(user.valid?).to eq(true)
+  end
+end

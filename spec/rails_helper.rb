43694b8982ca965cc155e1ebe8face66a214c1f8
Jason Lee <huacnlee@gmail.com>
2019-12-25 23:35:26 +0800

    Bundle update

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,9 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= "test"
+# Ensure use file uploader
+ENV["upload_provider"] = "file"
+
 require File.expand_path("../../config/environment", __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?

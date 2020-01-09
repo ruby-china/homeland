7f4d5978089a6e0aa2db8c3d44fc0b0ee51cd4b9
Jason Lee <huacnlee@gmail.com>
2020-01-10 00:00:14 +0800

    引入 auto-correct Gem，并对正文实现自动中英文排版格式化

--- app/models/topic/auto_correct.rb
+++ app/models/topic/auto_correct.rb
@@ -1,19 +1,9 @@
 # frozen_string_literal: true
 
-require "auto-space"
 
 class Topic
   module AutoCorrect
     extend ActiveSupport::Concern
-
-    CORRECT_CHARS = [
-      ["［", "["],
-      ["］", "]"],
-      ["【", "["],
-      ["】", "]"],
-      ["（", "("],
-      ["）", ")"]
-    ]
 
     included do
       before_save :auto_correct_title
@@ -22,11 +12,7 @@ class Topic
     private
       def auto_correct_title
         return if title.blank?
-        title.dup
-        CORRECT_CHARS.each do |chars|
-          title.gsub!(chars[0], chars[1])
-        end
-        title.auto_space!
+        self.title = ::AutoCorrect.format(title)
       end
   end
 end

c8216d9c4e8fe3070679f45cd4c3e55dafcb17c3
Jason Lee <huacnlee@gmail.com>
2020-01-31 00:40:38 +0800

    微调 CSS 细节

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -64,7 +64,7 @@ Doorkeeper.configure do
   # Define access token scopes for your provider
   # For more information go to
   # https://github.com/doorkeeper-gem/doorkeeper/wiki/Using-Scopes
-  # default_scopes  :public
+  default_scopes  :all
   # optional_scopes :write, :update
 
   # Change the way client credentials are retrieved from the request object.

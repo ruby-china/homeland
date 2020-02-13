36d5d43ef018b828b086ea0d2deb37d9542b6e5a
Jason Lee <huacnlee@gmail.com>
2020-02-14 01:00:41 +0800

    Avoid use Model in migration

--- db/migrate/20200213140928_upgrade_user_state.rb
+++ db/migrate/20200213140928_upgrade_user_state.rb
@@ -6,12 +6,6 @@ class UpgradeUserState < ActiveRecord::Migration[6.0]
     execute <<-SQL
       UPDATE users SET state = 3 WHERE verified = true
     SQL
-    admin_user_ids = User.where(email: Setting.admin_emails).pluck(:id)
-    if admin_user_ids.any?
-      execute <<-SQL
-        UPDATE users SET state = 99 WHERE id IN (#{admin_user_ids.join(",")})
-      SQL
-    end
   end
 
   def down

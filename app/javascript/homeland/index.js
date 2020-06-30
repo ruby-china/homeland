f3777ced93bfaa729accdae6c5b0857e721b98d6
Jason Lee <huacnlee@gmail.com>
2020-06-30 11:37:23 +0800

    Split webpack chunks

--- app/javascript/homeland/index.js
+++ app/javascript/homeland/index.js
@@ -14,7 +14,20 @@ Turbolinks.setProgressBarDelay(150);
 window.Rails = require('@rails/ujs');
 Rails.start();
 
-require("./vendor");
+require("pagination");
+require("jquery.timeago");
+require("jquery.timeago.settings");
+require("jquery.hotkeys");
+require("jquery.autogrow-textarea");
+require("tooltipster.bundle.min");
+require("dropzone");
+require("jquery.fluidbox.min");
+require("jquery.caret");
+require("jquery.atwho.min");
+require("google_analytics");
+require("jquery.infinitescroll.min");
+require("jquery.mobile-events");
+require("vendor/social-share-button");
 
 import { createConsumer } from '@rails/actioncable';
 
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,21 @@
 const { environment } = require('@rails/webpacker')
-const erb = require('./loaders/erb')
 
-environment.loaders.prepend('erb', erb)
+const extendConfig = {
+  devtool: false,
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          test: /node_modules|lib\/assets|vendor/,
+          name: 'vendors',
+          enforce: true,
+          chunks: 'all',
+        },
+      },
+    },
+  },
+};
+
+environment.config.merge(extendConfig);
+
 module.exports = environment

746d7f4d0e2b2c498d01092d90f504087ff841d7
Jason Lee <huacnlee@gmail.com>
2020-07-15 10:29:28 +0800

    bundle update

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -26,3 +26,10 @@ end
 
 
 SecondLevelCache.configure.cache_key_prefix = "slc:2"
+
+# FIXME: Upgrade redis-objects then remove this line.
+# `Redis#exists(key)` will return an Integer in redis-rb 4.3. `exists?` returns a boolean,
+# you should use it instead. To opt-in to the new behavior now you can set
+# Redis.exists_returns_integer =  true. To disable this message and keep the current (boolean) behaviour of 'exists'
+# you can set `Redis.exists_returns_integer = false`, but this option will be removed in 5.0.
+Redis.exists_returns_integer = false

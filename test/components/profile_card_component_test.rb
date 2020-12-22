f29b2bd1a4940b94440a27decb594f9282b6afdf
Jason Lee <huacnlee@gmail.com>
2020-12-22 22:52:36 +0800

    Add ViewComponent, and refactor ProfileCard, Topic as ViewComponent

---
+++ test/components/profile_card_component_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class ProfileCardComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    user = create(:user)
+    component = ProfileCardComponent.new(user: user)
+    component.stub(:owner?, true) do
+      doc = render_inline(component)
+      assert_equal user.name, doc.css(".user-profile-card .fullname").text.strip
+      assert_equal "@#{user.login}", doc.css(".user-profile-card .login").text.strip
+    end
+  end
+end

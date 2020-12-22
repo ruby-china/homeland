94ad77a2475f8d46dab74eb271ff68e41283e058
Jason Lee <huacnlee@gmail.com>
2020-12-22 17:00:53 +0800

    PumaWorkerKiller Max Memory limit including master worker

--- config/puma-master.rb
+++ config/puma-master.rb
@@ -14,7 +14,7 @@ on_worker_boot do
 end
 
 before_fork do
-  max_memory = (ENV["workers"] || 4).to_i * 380
+  max_memory = ((ENV["workers"] || 4).to_i + 1) * 380
   puts "=> Max Memory limit: #{max_memory}MB"
   PumaWorkerKiller.config do |config|
     config.ram           = max_memory  # mb

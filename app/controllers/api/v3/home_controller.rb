90be7a274910ebf867b6c6ae7fbb3621bf812419
aaronliu <935030733@qq.com>
2021-01-17 11:56:47 +0800

    feat: 重复判断

--- app/controllers/api/v3/home_controller.rb
+++ app/controllers/api/v3/home_controller.rb
@@ -4,9 +4,13 @@ module Api
       before_action :doorkeeper_authorize!, only: [:check_in, :show_check_in]
 
       def check_in
-        Rails.cache.write("check_in:user_id#{current_user.id}", Time.current.strftime("%F"))
-        current_user.change_score("signin")
-        render json: { success: true }
+        if current_user && !current_user&.show_signin?
+          render json: { success: false, message: "不能重复签到" }
+        else
+          Rails.cache.write("check_in:user_id#{current_user.id}", Time.current.strftime("%F"))
+          current_user.change_score("signin")
+          render json: { success: true }
+        end
       end
 
       def show_check_in
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,25 +18,18 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  #
+  if Rails.root.join("tmp", "caching-dev.txt").exist?
     config.action_controller.perform_caching = true
-      config.action_controller.enable_fragment_cache_logging = true
+    config.action_controller.enable_fragment_cache_logging = true
 
-      config.public_file_server.headers = {
-        "Cache-Control" => "public, max-age=#{2.days.to_i}"
-      }
-  # if Rails.root.join("tmp", "caching-dev.txt").exist?
-  #   config.action_controller.perform_caching = true
-  #   config.action_controller.enable_fragment_cache_logging = true
-  #
-  #   config.public_file_server.headers = {
-  #     "Cache-Control" => "public, max-age=#{2.days.to_i}"
-  #   }
-  # else
-  #   config.action_controller.perform_caching = false
-  #
-  #   config.cache_store = :null_store
-  # end
+    config.public_file_server.headers = {
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local

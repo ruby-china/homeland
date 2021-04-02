f77c38ecd7aa2e479eff3d286141e64e3c5a64c8
Jason Lee <huacnlee@gmail.com>
2021-04-02 15:00:14 +0800

    Improve TOC style

--- app/javascript/front/toc.js
+++ app/javascript/front/toc.js
@@ -5,7 +5,9 @@ window.TOCView = Backbone.View.extend({
   initialize(opts) {
     this.parentView = opts.parentView;
     const haveAnyHeaders = this.initHeadersInTopic();
-    if (haveAnyHeaders) { return $(".toc-container").show(); }
+    if (haveAnyHeaders) {
+      return $(".toc-container").show();
+    }
   },
 
   initHeadersInTopic() {
@@ -14,31 +16,26 @@ window.TOCView = Backbone.View.extend({
     }
     const markdownEl = $(".markdown-toc");
     markdownEl.prepend(`\
-<div class="toc-container dropdown pull-right">
+<div class="toc-container dropdown">
   <button data-toggle="dropdown" class="btn btn-default">
     <i class="fa fa-list"></i> 目录
   </button>
-  <div class="toc-panel dropdown-menu dropdown-menu-right">
-    <div class="list-container">
-      <ul class="list"></ul>
-    </div>
+  <div class="dropdown-menu dropdown-menu-right">
   </div>
 </div>\
-`
-    );
+`);
 
-    const items = $.map(markdownEl.find("h1,h2,h3,h4,h5,h6"), function(el, _) {
+    const items = $.map(markdownEl.find("h1,h2,h3,h4,h5,h6"), function (el, _) {
       const level = el.tagName.replace("H", "");
       const anchor = el.id;
-      return `<li class=\"toc-item toc-level-${level}\"> \
-<a href=\"#${encodeURI(anchor)}\" class=\"toc-item-link\">${el.textContent}</a> \
-</li>`;
+      const href = `#${encodeURI(anchor)}`;
+      return `<a href="${href}" class="dropdown-item toc-level-${level}">${el.textContent}</a>`;
     });
     if (items.length) {
-      $(".toc-container .list").html(items.join(""));
+      $(".toc-container .dropdown-menu").html(items.join(""));
       return true;
     } else {
       return false;
     }
-  }
+  },
 });

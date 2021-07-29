3697c25c35c851178b07bd936bb4ef0ae5e9804e
Jason Lee <huacnlee@gmail.com>
2021-07-29 10:12:26 +0800

    Add admin delete recent notes button

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -88,6 +88,8 @@ module Admin
         _clean_topics
       when "photos"
         _clean_photos
+      when "notes"
+        _clean_notes
       end
     end
 
@@ -113,6 +115,15 @@ module Admin
       Photo.unscoped.where(user_id: @user.id).recent.limit(10).destroy_all
       count = Photo.unscoped.where(user_id: @user.id).count
       redirect_to edit_admin_user_path(@user.id), notice: "Recent 10 photos has been deleted successfully, now #{@user.login} still #{count} photos"
+    end
+
+    def _clean_notes
+      count = 0
+      if defined? Note
+        Note.unscoped.where(user_id: @user.id).recent.limit(10).destroy_all
+        count = Note.unscoped.where(user_id: @user.id).count
+      end
+      redirect_to edit_admin_user_path(@user.id), notice: "Recent 10 notes has been deleted successfully, now #{@user.login} still #{count} notes"
     end
   end
 end

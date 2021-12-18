eb8fba48ef5928d3e6ca64535bcc8de60f3d6cb0
Jason Lee <huacnlee@gmail.com>
2021-12-18 15:02:26 +0800

    Upgrade Npms for try to fix Webpacker precompile in production env.

--- babel.config.js
+++ babel.config.js
@@ -23,9 +23,7 @@ module.exports = function (api) {
           targets: {
             node: "current",
           },
-          modules: "commonjs",
         },
-        "@babel/preset-react",
       ],
       (isProductionEnv || isDevelopmentEnv) && [
         "@babel/preset-env",
@@ -35,13 +33,6 @@ module.exports = function (api) {
           corejs: 3,
           modules: false,
           exclude: ["transform-typeof-symbol"],
-        },
-      ],
-      [
-        "@babel/preset-react",
-        {
-          development: isDevelopmentEnv || isTestEnv,
-          useBuiltIns: true,
         },
       ],
     ].filter(Boolean),
@@ -63,24 +54,27 @@ module.exports = function (api) {
         },
       ],
       [
+        "@babel/plugin-proposal-private-methods",
+        {
+          loose: true,
+        },
+      ],
+      [
+        "@babel/plugin-proposal-private-property-in-object",
+        {
+          loose: true,
+        },
+      ],
+      [
         "@babel/plugin-transform-runtime",
         {
           helpers: false,
-          regenerator: true,
-          corejs: false,
         },
       ],
       [
         "@babel/plugin-transform-regenerator",
         {
           async: false,
-        },
-      ],
-      ["@babel/plugin-proposal-private-methods", { loose: true }],
-      isProductionEnv && [
-        "babel-plugin-transform-react-remove-prop-types",
-        {
-          removeImport: true,
         },
       ],
     ].filter(Boolean),

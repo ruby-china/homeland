f147aeef7a16825bd572d4b294d14a5e7640c683
Jason Lee <huacnlee@gmail.com>
2023-01-10 19:30:23 +0800

    Default page size change from 25 to 30

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Kaminari.configure do |config|
-  config.default_per_page = 25
+  config.default_per_page = 30
   # config.max_per_page = nil
   config.window = 2
   config.outer_window = 2

4d4f27bc990685d7515f9b78b01c0f7b991596bd
Jason Lee <huacnlee@gmail.com>
2023-09-18 18:49:04 +0800

    Imporve process topic referneces by use async.

---
+++ app/jobs/topic_reference_job.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class TopicReferenceJob < AsyncJob
+  REFERENCE_PATTERN = %r{/topics/(\d+)}
+
+  def perform(item_type, item_id)
+    item = item_type.constantize.find_by(id: item_id)
+    return if item&.body.blank?
+
+    matched_topic_ids = []
+    item.body.gsub(REFERENCE_PATTERN) do
+      topic_id = Regexp.last_match(1)
+      matched_topic_ids << topic_id if topic_id.present?
+    end
+    matched_topic_ids.uniq!
+
+    referer_id = item.is_a?(Topic) ? item.id : item.topic_id
+    # Avoid reference Topic itself or Reply's Topic
+    matched_topic_ids.delete(referer_id.to_s)
+    exist_ids = Topic.where(id: matched_topic_ids).pluck(:id)
+
+    Topic.transaction do
+      exist_ids.each do |target_id|
+        Topic.create_action(:reference, target_type: "Topic", target_id: target_id, user_type: "Topic", user_id: referer_id)
+      end
+    end
+  end
+end
--- test/models/concerns/topic_reference_test.rb
+++ test/models/concerns/topic_reference_test.rb
@@ -1,40 +1,46 @@
 # frozen_string_literal: true
 
-require "spec_helper"
+require "test_helper"
 
 class TopicReferenceTest < ActiveSupport::TestCase
+  include ActiveJob::TestHelper
+
   test "topic" do
-    t1 = create(:topic)
-    t2 = create(:topic)
-    t3 = create(:topic)
+    perform_enqueued_jobs do
+      t1 = create(:topic)
+      t2 = create(:topic)
+      t3 = create(:topic)
 
-    t1.update(body: "hello /topics/#{t2.id} and /topics/#{t3.id} /topics/#{t2.id} /topics/#{t1.id} /topics/111222222222222")
-    t1.reload
-    assert_equal 2, t1.reference_topic_ids.count
-    assert_equal [t2.id, t3.id], t1.reference_topic_ids.sort
+      t1.update(body: "hello /topics/#{t2.id} and /topics/#{t3.id} /topics/#{t2.id} /topics/#{t1.id} /topics/0123456789")
+      t1.reload
+      assert_equal 2, t1.reference_topic_ids.count
+      assert_equal [t2.id, t3.id], t1.reference_topic_ids.sort
 
-    # When update body, should keep the old reference
-    t1.update(body: "hello /topics/#{t2.id}")
-    t1.reload
-    assert_equal [t2.id, t3.id], t1.reference_topic_ids.sort
+      # When update body, should keep the old reference
+      t1.update(body: "hello /topics/#{t2.id}")
+      t1.reload
+      assert_equal [t2.id, t3.id], t1.reference_topic_ids.sort
+    end
   end
 
   test "reply" do
-    t = create(:topic)
-    t2 = create(:topic)
-    r1 = create(:reply)
-    r2 = create(:reply, body: "hello /topics/#{t.id} /topics/#{t2.id}", topic: t2)
+    perform_enqueued_jobs do
+      t = create(:topic)
+      t2 = create(:topic)
+      r1 = create(:reply)
+      r2 = create(:reply, body: "hello /topics/#{t.id} /topics/#{t2.id}", topic: t2)
 
-    r2.topic.reload
-    assert_equal 1, r2.topic.reference_topic_ids.count
-    assert_equal [t.id], r2.topic.reference_topic_ids
+      r2.topic.reload
+      assert_equal 1, r2.topic.reference_topic_ids.count
+      assert_equal [t.id], r2.topic.reference_topic_ids
 
-    r1.update(body: "hello /topics/#{t.id}")
-    r1.reload
-    assert_equal 1, r1.topic.reference_topic_ids.count
+      r1.update(body: "hello /topics/#{t.id}")
+      r1.reload
+      assert_equal 1, r1.topic.reference_topic_ids.count
 
-    t.reload
-    assert_equal 2, t.reference_by_topics.count
-    assert_equal [t2.id, r1.topic_id].sort, t.reference_by_topics.pluck(:id).sort
+      t.reload
+      assert_equal 2, t.reference_by_topics.count
+      assert_equal [t2.id, r1.topic_id].sort, t.reference_by_topics.pluck(:id).sort
+    end
   end
 end

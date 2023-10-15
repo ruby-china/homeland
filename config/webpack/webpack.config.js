c64b4ca5a465d7b2912cae9249aa584331fdb5e9
Eric-Guo <eric.guocz@gmail.com>
2023-10-15 14:51:58 +0800

    webpackConfig now need generated.
    https://github.com/shakacode/shakapacker/blob/master/docs/v7_upgrade.md#example-upgrade

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,6 +1,7 @@
-const { webpackConfig } = require('shakapacker');
+const { generateWebpackConfig } = require('shakapacker');
 const { merge } = require('webpack-merge');
 const webpack = require("webpack");
+const webpackConfig = generateWebpackConfig();
 
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
 const customConfig = {

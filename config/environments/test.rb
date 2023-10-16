b1237a81e00e2b2b110cc87e4c9476509bc254d4
Jason Lee <huacnlee@gmail.com>
2023-10-16 13:40:25 +0800

    Update `config.enable_reloading = false` in test env, use Rails default.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # While tests run files are not watched, reloading is not necessary.
-  config.enable_reloading = true
+  config.enable_reloading = false
 
   # Eager loading loads your entire application. When running a single test locally,
   # this is usually not necessary, and can slow down your test suite. However, it's

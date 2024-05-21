d18e97d2088caee82e62a88b27c5e8e00bd6b5ff
Jason Lee <huacnlee@gmail.com>
2024-05-21 21:59:51 +0800

    Update `Dotenv::Railtie` to `Dotenv::Rails`. (#1412)

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+Dotenv::Rails.load
 
 module Homeland
   class Application < Rails::Application

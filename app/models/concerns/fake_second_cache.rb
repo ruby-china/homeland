9085935cb4e45785f1971554f2d755192eb89388
Jason Lee <huacnlee@gmail.com>
2024-10-29 13:52:35 +0800

    Fix `second_level_cache` method not found error. (#1440)

---
+++ app/models/concerns/fake_second_cache.rb
@@ -0,0 +1,9 @@
+# Add a fake second_level_cache method to the model for compatibility with the Homeland plugins.
+module FakeSecondCache
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def second_level_cache(args)
+    end
+  end
+end

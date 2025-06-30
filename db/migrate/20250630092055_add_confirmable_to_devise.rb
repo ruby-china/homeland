a823558c61949d2c7203912652aac8b6edd4b3fa
Jason Lee <huacnlee@gmail.com>
2025-06-30 19:08:44 +0800

    Fix migration error by use sql execute.

--- db/migrate/20250630092055_add_confirmable_to_devise.rb
+++ db/migrate/20250630092055_add_confirmable_to_devise.rb
@@ -8,7 +8,9 @@ class AddConfirmableToDevise < ActiveRecord::Migration[8.0]
       # User.reset_column_information # Need for some types of updates, but not for update_all.
       # To avoid a short time window between running the migration and updating all existing
       # users as confirmed, do the following
-      User.update_all confirmed_at: DateTime.now
+      execute <<-SQL
+        UPDATE users SET confirmed_at = '#{DateTime.now}'
+      SQL
       # All existing user accounts should be able to log in after this.
     end
 
